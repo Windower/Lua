fcdb4d3aa56c081dcc543735b9022a9bc00f6de6
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-02-26 19:11:33 +0100

    Added Linker add-on

---
+++ addons/Linker/Linker.lua
@@ -0,0 +1,68 @@
+-- Interpreter
+
+function event_addon_command(command, ...)
+    term = table.concat({...}, ' ')
+	if(term == '') then
+		os.execute('start '..rawURLs[command])
+	else
+		os.execute('start '..string.gsub(searchURLs[command], 'QUERYTERM', term))
+	end
+end
+
+-- Constructor
+
+function event_load()
+	searchURLs = {}
+	-- FFXI info sites
+	searchURLs.db = 'http://ffxidb.com/search?q=QUERYTERM'
+	searchURLs.ah = 'http://ffxiah.com/search/item?q=QUERYTERM'
+	searchURLs.bgw = 'http://wiki.bluegartr.com/index.php?title=Special:Search&search=QUERYTERM'
+	searchURLs.ge = 'http://ffxi.gamerescape.com/wiki/Special:Search?search=QUERYTERM'
+	searchURLs.wikia = 'http://wiki.ffxiclopedia.org/wiki/index.php?search=QUERYTERM&fulltext=Search'
+	-- Miscallenous links
+	searchURLs.g = 'http://google.com/q=QUERYTERM'
+	searchURLs.wa = 'http://wolframalpha.com/i=QUERYTERM'
+	
+	rawURLs = {}
+	-- FFXI info sites
+	rawURLs.db = 'http://ffxidb.com/'
+	rawURLs.ah = 'http://ffxiah.com/'
+	rawURLs.bgw = 'http://wiki.bluegartr.com/bg/Main_Page'
+	rawURLs.ge = 'http://ffxi.gamerescape.com/wiki/Main_Page'
+	rawURLs.wikia = 'http://wiki.ffxiclopedia.org/wiki/Main_Page'
+	-- FFXI forums
+	rawURLs.of = 'http://forum.square-enix.com/ffxi/forum.php'
+	rawURLs.bgf = 'http://www.bluegartr.com/forum.php'
+	rawURLs.ahf = 'http://www.ffxiah.com/forum'
+	-- Miscallenous links
+	rawURLs.g = 'http://google.com'
+	rawURLs.wa = 'http://wolframalpha.com'
+	
+	send_command('alias db lua c Linker db')
+	send_command('alias ah lua c Linker ah')
+	send_command('alias bgw lua c Linker bgw')
+	send_command('alias ge lua c Linker ge')
+	send_command('alias wikia lua c Linker wikia')
+	send_command('alias g lua c Linker g')
+	send_command('alias wa lua c Linker wa')
+	send_command('alias of lua c Linker of')
+	send_command('alias bgf lua c Linker bgf')
+	send_command('alias ahf lua c Linker ahf')
+end
+
+-- Destructor
+
+function event_unload()
+	searchURLs = nil
+	rawURLs = nil
+	send_command('unalias db')
+	send_command('unalias ah')
+	send_command('unalias bgw')
+	send_command('unalias ge')
+	send_command('unalias wikia')
+	send_command('unalias g')
+	send_command('unalias wa')
+	send_command('unalias of')
+	send_command('unalias bgf')
+	send_command('unalias ahf')
+end
---
+++ addons/Linker/readme.md
@@ -0,0 +1,19 @@
+Opens certain links via simple console commands, with an optional search string. If the search string is omitted, the home page of that site will open.
+
+# Commands
+
+## FFXI info sites:
+    //db [query] # FFXIDB
+	//ah [query] # FFXIAH
+	//bgw [query] # BlueGartr Wiki
+	//ge [query] # GamerEscape Wiki
+	//wikia [query] # FFXIclopedia
+
+## FFXI community sites
+	//of # Official forums
+	//bgf # BlueGartr forums
+	//ahf # FFXIAH forums
+
+## Miscellanous sites
+	//g [query] # Google
+	//wa [query] # Wolfram|Alpha
