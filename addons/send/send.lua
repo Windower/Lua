2b0f11edce7b26501edf144333accda8329460a9
Aureus <andrew.todd@gmail.com>
2013-03-09 16:36:04 -0500

    add @job, @all, @other arguments, add tag parsing

--- addons/send/send.lua
+++ addons/send/send.lua
@@ -14,6 +14,8 @@ end
 function event_ipc_message(msg)
 	broken = split(msg, ' ')
 	
+	if broken:getn() < 2 then return end
+	
 	qual = table.remove(broken,1)
 	player = get_player()
 	if qual:lower()==player["name"]:lower() then
@@ -21,11 +23,23 @@ function event_ipc_message(msg)
 			relevant_msg(table.concat(broken,' '))
 		end
 	end
-	if qual:upper() == player["main_job"]:upper() then
-		if broken ~= nil then
-			relevant_msg(table.concat(broken,' '))
+	if string.char(qual:byte(1)) == '@' then
+		arg = string.char(qual:byte(2, qual:len())
+		if arg:upper() == player["main_job"]:upper() then
+			if broken ~= nil then
+				relevant_msg(table.concat(broken,' '))
+			end
+		elseif arg:upper == 'ALL' then
+			if broken ~= nil then
+				relevant_msg(table.concat(broken,' '))
+			end
+		elseif arg:upper == 'OTHERS' then
+			if broken ~= nil then
+				relevant_msg(table.concat(broken,' '))
+			end
 		end
 	end
+
 end
 
 function split(msg, match)
@@ -50,6 +64,18 @@ function split(msg, match)
 end
 
 function relevant_msg(msg)
+	local player = get_player()
+	msg:gsub("<me>", player['name'])
+	msg:gsub("<hp>", tostring(player['hp']))
+	msg:gsub("<mp>", tostring(player['mp']))
+	msg:gsub("<hpp>", tostring(player['hpp']))
+	msg:gsub("<mpp>", tostring(player['mpp']))
+	msg:gsub("<tp>", tostring(player['tp']))
+	msg:gsub("<job>", player['main_job_full']..'/'..player['sub_job_full'])
+	msg:gsub("<mjob>", player['main_job_full'])
+	msg:gsub("<sjob>", player['sub_job_full'])
+	
+
 	if msg:sub(1,1)=='/' then
 		send_command('input '..msg)
 	else
