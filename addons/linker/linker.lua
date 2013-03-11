7afa7ce708a252a21af0efda666175855eb38f1b
Aureus <andrew.todd@gmail.com>
2013-03-11 16:46:51 -0400

    Fix linker name case

---
+++ addons/linker/linker.lua
@@ -0,0 +1,70 @@
+-- Helpers
+
+function log(...)
+	local args = {...}
+	local strtable = {}
+	for i = 1, #args, 1 do
+		strtable[i] = tostring(args[i])
+	end
+	add_to_chat(160, table.concat(strtable, ' '))
+end
+
+function round(num, prec)
+	local mult = 10^(prec or 0)
+	return math.floor(num * mult + 0.5) / mult
+end
+
+-- Interpreter
+
+function event_addon_command(site, ...)
+	term = table.concat({...}, ' ')
+	if((term == '') or (searchURLs[site] == nil and rawURLs[site] ~= nil)) then
+		link = rawURLs[site]
+	elseif(searchURLs[site] ~= nil) then
+		link = searchURLs[site]:gsub('QUERYTERM', term)
+	else
+		log('Linker error:', 'Command', site, 'not found.')
+	end
+	if link ~= nil then
+		open_url(link)
+	end
+end
+
+-- Constructor
+
+function event_load()
+	searchURLs = {}
+	-- FFXI info sites
+	searchURLs['db'] = 'http://ffxidb.com/search?q=QUERYTERM'
+	searchURLs['ah'] = 'http://ffxiah.com/search/item?q=QUERYTERM'
+	searchURLs['bg'] = 'http://wiki.bluegartr.com/index.php?title=Special:Search&search=QUERYTERM'
+	searchURLs['ge'] = 'http://ffxi.gamerescape.com/wiki/Special:Search?search=QUERYTERM'
+	searchURLs['wikia'] = 'http://wiki.ffxiclopedia.org/wiki/index.php?search=QUERYTERM&fulltext=Search'
+	-- Miscallenous sites
+	searchURLs['g'] = 'http://google.com/?q=QUERYTERM'
+	searchURLs['wa'] = 'http://wolframalpha.com/?i=QUERYTERM'
+	
+	rawURLs = {}
+	-- FFXI info sites
+	rawURLs['db'] = 'http://ffxidb.com/'
+	rawURLs['ah'] = 'http://ffxiah.com/'
+	rawURLs['bg'] = 'http://wiki.bluegartr.com/bg/Main_Page'
+	rawURLs['ge'] = 'http://ffxi.gamerescape.com/wiki/Main_Page'
+	rawURLs['wikia'] = 'http://wiki.ffxiclopedia.org/wiki/Main_Page'
+	-- FFXI community sites
+	rawURLs['of'] = 'http://forum.square-enix.com/ffxi/forum.php'
+	rawURLs['bgf'] = 'http://www.bluegartr.com/forum.php'
+	rawURLs['ahf'] = 'http://www.ffxiah.com/forum'
+	rawURLs['gw'] = 'http://guildwork.com'
+	-- Miscallenous sites
+	rawURLs['g'] = 'http://google.com'
+	rawURLs['wa'] = 'http://wolframalpha.com'
+	
+	send_command('alias web lua c Linker')
+end
+
+-- Destructor
+
+function event_unload()
+	send_command('unalias web')
+end
