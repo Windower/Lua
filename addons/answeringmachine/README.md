ece4fd38fcc136af75fa1a0282b3b55e229cb170
Byrth Noth <squarefocus@hotmail.com>
2013-03-11 23:29:48 -0400

    Semi-major changes
    You can now set a functional away message and clear messages from
    specific people. It is also no longer case sensitive.

--- addons/answeringmachine/README.md
+++ addons/answeringmachine/README.md
@@ -1,15 +1,15 @@
 Author: Byrth
-Version: 0.1
+Version: 1.0
 Answering Machine with basic functionality
 
 Abbreviation: //am
 
 Commands:
-* list : Lists available messages
+* list : Lists the number of messages recorded from each person that has sent you a tell.
 * play <name> : Plays available messages. Will default to playing all messages if a name is not provided.
-* clear : Clears all of the messages in your inbox.
+* clear <name> : Clears available messages. Will default to clearing all messages if a name is not provided.
 * help : Lists commands in game.
-* msg <message> : Sets your away message, which will be sent to non-GMs the first time they send you a tell after loading the plugin.
+* msg <message> : Sets your away message, which will be sent to non-GMs the first time they send you a tell after loading the plugin or clearing messages from them.
 
 Purpose:
 To record tells received while you are afk and play them back at your leisure.
--- addons/answeringmachine/answeringmachine.lua
+++ addons/answeringmachine/answeringmachine.lua
@@ -3,59 +3,70 @@ function event_addon_command(...)
 	local broken = split(term, ' ')
 	if broken[1] ~= nil then
 		if broken[1]:upper() == "CLEAR" then
-			tell_table = {}
-			recording = {}
-			add_to_chat(4,'Answering Machine>> Blanking the recordings')
+			if broken[2] == nil then
+				tell_table = {}
+				recording = {}
+				add_to_chat(4,'Answering Machine>> Blanking the recordings')
+			elseif tell_table[broken[2]:upper()]~=nil then
+				add_to_chat(4,'Answering Machine>> Deleting messages from '..uc_first(broken[2]))
+				tell_table[broken[2]:upper()]=nil
+				recording[broken[2]:upper()]=nil
+			else
+				add_to_chat(5,'Cancel error: Could not find specified player in tell history')
+			end
 		end
 		
 		if broken[1]:upper() == "LIST" then
 			for i,v in pairs(tell_table) do
-				add_to_chat(5,v..' messages from '..i)
+				add_to_chat(5,v..' messages from '..uc_first(i))
 			end
 		end
 
 		if broken[1]:upper() == "PLAY" then
 			if broken[2] ~= nil then
-				if tell_table[broken[2]] ~= nil then
-					local num = tell_table[broken[2]]
+				if tell_table[broken[2]:upper()] ~= nil then
+					local num = tell_table[broken[2]:upper()]
 					if num == 1 then
-						add_to_chat(5,'1 message from '..broken[2])
+						add_to_chat(5,'1 message from '..uc_first(broken[2]))
 					else
-						add_to_chat(5,num..' messages from '..broken[2])
+						add_to_chat(5,num..' messages from '..uc_first(broken[2]))
 					end
 					for n = 1,num do
-						local tablekey = recording[broken[2]]
-						add_to_chat(4,broken[2]..'>> '..tablekey[n])
+						local tablekey = recording[broken[2]:upper()]
+						add_to_chat(4,uc_first(broken[2])..'>> '..tablekey[n])
 					end
 				end
 			else
 				add_to_chat(4,'Answering Machine>> Playing back all messages')
 				for i,v in pairs(tell_table) do
 					if v == 1 then
-						add_to_chat(5,'1 message from '..i)
+						add_to_chat(5,'1 message from '..uc_first(i))
 					else
-						add_to_chat(5,v..' messages from '..i)
+						add_to_chat(5,v..' messages from '..uc_first(i))
 					end
 					for n = 1,v do
 						local tablekey = recording[i]
-						add_to_chat(4,i..'>> '..tablekey[n])
+						add_to_chat(4,uc_first(i)..'>> '..tablekey[n])
 					end
 				end
 			end
 		end
 		
 		if broken[1]:upper() == "HELP" then
-			write('am clear : Clears all messages')
-			write('am play <name> : Plays current messages, or only messages from <name> if provided')
+			write('am clear <name> : Clears current messages, or only messages from <name> if provided')
+			write('am help : Lists these commands!')
 			write('am list : Lists the names of people who have sent you tells')
+			write('am msg <message> : Sets your away message, which will be sent to non-GMs only once after plugin load or message clear')
+			write('am play <name> : Plays current messages, or only messages from <name> if provided')
 		end
 		
 		
 		if broken[1]:upper() == "MSG" then
 			local msg_interp = broken
-			msg_interp[1]=nil
+			table.remove(msg_interp,1)
 			if msg_interp ~= nil then
-				away_msg=table.concat(msg_interp)
+				away_msg=table.concat(msg_interp,' ')
+				add_to_chat(5,'Message set to: '..away_msg)
 			end
 		end
 	end
@@ -73,13 +84,13 @@ end
 
 function event_chat_message(isGM, mode, player, message)
 	if mode==3 then
-		if tell_table[player] ~= nil then
-			tell_table[player] = tell_table[player]+1
-			local playertab = recording[player]
+		if tell_table[player:upper()] ~= nil then
+			tell_table[player:upper()] = tell_table[player:upper()]+1
+			local playertab = recording[player:upper()]
 			playertab[#playertab+1] = message
 		else
-			tell_table[player] = 1
-			recording[player] = {message}
+			tell_table[player:upper()] = 1
+			recording[player:upper()] = {message}
 			if away_msg ~= nil then
 				if isGM ~= 1 then
 					send_command('input /tell '..player..' '..away_msg)
@@ -108,4 +119,11 @@ function split(msg, match)
 		end
 	end
 	return splitarr
+end
+
+function uc_first(msg)
+	local length = msg:len()
+	local first_char = msg:sub(1,1)
+	local rest = msg:sub(2,length)
+	return first_char:upper()..rest:lower()
 end
