a83e0bd8019059e3430ab3a3abd3bfe85c9eaff9
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-03-12 13:48:03 +0100

    Added print() and vprint() to tables

--- addons/libs/debug.lua
+++ addons/libs/debug.lua
@@ -7,5 +7,37 @@ function log(...)
 end
 
 function table.print(t)
-	log(unpack(t))
+	t = T(t)
+	t = t:map(tostring)
+	tstr = ''
+	if #t == 0 then
+		first = true
+		for key, val in pairs(t) do
+			if(first) then
+				first = false
+			else
+				tstr = tstr..', '
+			end
+			tstr = tstr..key..'='..val
+		end
+	else
+		tstr = t:concat(', ')
+	end
+	log('{'..tstr..'}')
+end
+
+function table.vprint(t)
+	t = T(t)
+	t = t:map(tostring)
+	log('{')
+	if #t == 0 then
+		for key, val in pairs(t) do
+			log('    '..key..'='..val)
+		end
+	else
+		for key = 1, #t do
+			log('    '..t[key])
+		end
+	end
+	log('}')
 end
