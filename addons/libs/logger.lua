479b00dc85e978cba65ecbb7302e02bac4223902
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-03-14 01:06:11 +0100

    Added various library functions
    Added to logger:
    log(...) - will now include printing of nil values
    table.tostring(t) - returns string representation of table values in Lua
    syntax
    table.tovstring(t) - returns vertical string representation of table
    values in Lua syntax with
    Amended table.print(t) and table.vprint(t) to handle nested tables
    
    Added to stringhelper:
    string.trim(str) - removes leading and trailing whitespace-like
    characters
    string.uc_first(str) - returns str with the first letter capitalized
    string.capitalize(str) - returns str with the first letters of every
    word capitalized
    Fixed string.split(str, sep)
    Fixed string.add(str, pos)
    Amended string.split(str, sep, maxsplit) - takes optional third argument
    for maximum resulting table length
    
    Added to tablehelper:
    table.extend(t, t_ext) - appends table t_ext to the end of t
    table.arrmap(t, fn) - analogon to table.map for array tables, handles
    enclosed nil values
    Restricted exposure of meta table data

--- addons/libs/logger.lua
+++ addons/libs/logger.lua
@@ -3,49 +3,87 @@ This library provides a set of functions to aid in debugging.
 ]]
 
 require 'tablehelper'
+require 'stringhelper'
 
 -- Prints the arguments provided to the FFXI chatlog, in the same color used for Campaign/Bastion alerts and Kupower messages. Can be changed below.
 -- Converts any kind of object type to a string, so it's type-safe.
 -- Concatenates all provided arguments with whitespaces.
 function log(...)
-	local args = T{...}
-	local strtable = T(args:map(tostring))
-	add_to_chat(160, strtable:sconcat())
+	add_to_chat(160, T{...}:arrmap(tostring):sconcat())
 end
 
--- Prints a table in explicit Lua syntax: {...}
-function table.print(t)
+-- Returns a string representation of a table in explicit Lua syntax: {...}
+function table.tostring(t)
 	-- Convert all values to strings, to make sure everything is ready for string concatenation.
-	t = T(t):map(tostring)
-	tstr = ''
+	local t = T(t)
+	local tstr = ''
 	
 	-- Iterate over table.
 	for key, val in pairs(t) do
-		-- Append to the string.
-		tstr = tstr..key..'='..val
+		-- Check for nested tables
+		if type(val) == 'table' then
+			valstr = T(val):tostring()
+		else
+			valstr = tostring(val)
+		end
 		
-		-- Add commata, unless it's the last value.
+		-- Append to the string.
+		tstr = tstr..key..'='..valstr
+		
+		-- Add comma, unless it's the last value.
 		if next(t, key) ~= nil then
 			tstr = tstr..', '
 		end
 	end
 	
 	-- Output the result, enclosed in braces.
-	log('{'..tstr..'}')
+	return '{'..tstr..'}'
 end
 
--- Prints a table vertically in explicit Lua syntax, with every element in its own line:
+-- Prints a string representation of a table in explicit Lua syntax: {...}
+function table.print(t)
+	log(T(t):tostring())
+end
+
+-- Returns a vertical string representation of a table in explicit Lua syntax, with every element in its own line:
+--- {
+---     ...
+--- }
+function table.tovstring(t, indentlevel)
+	indentlevel = indentlevel or 0
+	-- Convert all values to strings, to make sure everything is ready for string concatenation.
+	local t = T(t)
+	local tstr = ''
+	
+	local indent = (' '):rep(indentlevel*4)
+	tstr = tstr..indent..'{'.."\n"
+	for key, val in pairs(t) do
+		-- Check for nested tables
+		if type(val) == 'table' then
+			valstr = T(val):tovstring(indentlevel+1)
+		else
+			valstr = tostring(val)
+		end
+		
+		-- Append one line with indent.
+		tstr = tstr..indent..'    '..key..'='..valstr
+		
+		-- Add comma, unless it's the last value.
+		if next(t, key) ~= nil then
+			tstr = tstr..', '
+		end
+		
+		tstr = tstr.."\n"
+	end
+	tstr = tstr..indent..'}'
+	
+	return tstr
+end
+
+-- Prints a vertical string representation of a table in explicit Lua syntax, with every element in its own line:
 --- {
 ---     ...
 --- }
 function table.vprint(t)
-	-- Convert all values to strings, to make sure everything is ready for string concatenation.
-	t = T(t):map(tostring)
-	
-	log('{')
-	for key, val in pairs(t) do
-		-- Output one line with indent.
-		log('    '..key..'='..val)
-	end
-	log('}')
+	T(t):tovstring():split("\n"):arrmap(log)
 end
