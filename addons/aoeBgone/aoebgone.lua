d1d59b2b6ab9e2273c137dbc4820afbd637f71b7
Ricky Gall <nitrous24@gmail.com>
2013-03-15 02:26:53 -0400

    Added cor roll support.

--- addons/aoeBgone/aoebgone.lua
+++ addons/aoeBgone/aoebgone.lua
@@ -17,7 +17,7 @@ function event_incoming_text(original, modified, color)
 	local targetchar
 	local effect
 	a,b,targetchar,effect = string.find(original,"([%w]+) gains the effect of ([%w%s%c]+)\46")
-	
+	c,d,tar2,eff2 = string.find(original,"([%w]+) receives the effect of ([%w]+).* Roll.\46")
 	if effect~=nil then
 		if stat_array[effect..'send_single'] ~= 1 then
 			if stat_array[effect]==nil then
@@ -42,13 +42,47 @@ function event_incoming_text(original, modified, color)
 			modified = original
 			stat_array[effect..'send_single'] = nil
 		end
+	elseif eff2~=nil then
+		g,h,app,total = string.find(original,"The total for %w+(.*) Roll increases to ([%d]+).\46")
+		roll = 1
+		if total ~= nil then
+			du = total
+			ap = app
+		end
+		if stat_array[eff2..'send_single'] ~= 1 then
+			if stat_array[eff2]==nil then
+				local lines = split(original,'\7')
+				if stat_array[eff2]~=nil then
+					write(stat_array[eff2])
+				end
+				stat_array[eff2]={lines[1], color}
+				local delay = 0
+				if slow_spells[eff2]~=nil then
+					delay = 5
+				else
+					delay = 2
+				end
+				send_command('wait '..delay..';lua c aoebgone Send it out '..eff2..'5')
+			end
+			local j=stat_array[eff2]
+			j[#j+1]=tar2
+			
+			modified = ''
+		else
+			modified = original
+			stat_array[effect..'send_single'] = nil
+		end
 	end
 	
 	return modified, color
 end
 
 function send_it_out(n)
-	output = stat_array[n][1]..'\7'..stat_array[n][3]
+	if du ~= nil then
+		output = stat_array[n][1]..'\7The total for '..n..ap..' Roll increases to '..du..'!\7'..stat_array[n][3]
+	else
+		output = stat_array[n][1]..'\7'..stat_array[n][3]
+	end
 	for i,v in pairs(stat_array[n]) do
 		if i > 3 then
 			if i <= #stat_array[n]-1 then
@@ -60,13 +94,33 @@ function send_it_out(n)
 		end
 	end
 	col = stat_array[n][2]
-	if #stat_array[n]>3 then
+	if roll ~= nil then
+		if #stat_array[n]>3 then
+			stat_array[n]=nil
+			if total ~= nil then
+				add_to_chat(col,output..' receive the effect of '..n.."'s Roll.")
+			else
+				add_to_chat(col,output..' receive the effect of '..n.."'s Roll.")
+			end
+		else
+			stat_array[n]=nil
+			stat_array[n..'send_single']=1
+			if total ~= nil then
+				add_to_chat(col,output..' receives the effect of '..n.."'s Roll.")
+			else
+				add_to_chat(col,output..' receives the effect of '..n.."'s Roll.")
+			end	
+		end
+		du = nil
+	else
+		if #stat_array[n]>3 then
 		stat_array[n]=nil
 		add_to_chat(col,output..' gain the effect of '..n..'.')
-	else
-		stat_array[n]=nil
-		stat_array[n..'send_single']=1
-		add_to_chat(col,output..' gains the effect of '..n..'.')
+		else
+			stat_array[n]=nil
+			stat_array[n..'send_single']=1
+			add_to_chat(col,output..' gains the effect of '..n..'.')
+		end
 	end
 end
 
