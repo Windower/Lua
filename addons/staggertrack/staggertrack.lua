b085f6b0b0e7f371501a5dc9767846a9c9fd4b1a
Nitrous <nitrous24@gmail.com>
2013-04-05 17:14:57 -0300

    Update staggertrack.lua

--- addons/staggertrack/staggertrack.lua
+++ addons/staggertrack/staggertrack.lua
@@ -1,5 +1,5 @@
 --[[
-Stagger Tracker v1.5
+Stagger Tracker v1.51
 Copyright (c) 2013, Ricky Gall All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
