f6e96bcc59de6809fc16a5de752456d911a91148
Podginator <Podginator@gmail.com>
2013-04-16 22:15:44 +0100

    SAM roll.

--- addons/rolltracker/rolltracker.lua
+++ addons/rolltracker/rolltracker.lua
@@ -66,7 +66,7 @@ function event_load()
 	roll_ident={[97]=' ', ['98']='Fighter\'s',['99']='MNK',['100']='WHM',
 						['101']='Wizard\'s',['102']='Warlock\'s',['103']='Rogue\'s',
 						['104']='Gallant\'s',['105']='Chaos',['106']='Beast',
-						['107']='Choral',['108']='Hunter\'s',['109']='Samurai\'s',
+						['107']='Choral',['108']='Hunter\'s',['109']='Samurai',
 						['110']='Ninja',['111']='Drachen',['112']='Evoker\'s',
 						['113']='Magus\'',['114']='Corsair\'s',['115']='Puppet',
 						['116']='Dancer\'s',['117']='Scholar\'s',['118']='Bolter\'s',
