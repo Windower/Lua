3cc4ce9dbf9b475f9ee23e48be6dca1bae2e8549
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-04-20 19:58:31 +0200

    filehelper: Less dependencies and added line iterator

--- addons/libs/filehelper.lua
+++ addons/libs/filehelper.lua
@@ -4,11 +4,9 @@ File handler.
 
 _libs = _libs or {}
 _libs.filehelper = true
-_libs.tablehelper = _libs.tablehelper or require 'tablehelper'
 _libs.stringhelper = _libs.stringhelper or require 'stringhelper'
-_libs.logger = _libs.logger or require 'logger'
 
-local file = T{}
+local file = {}
 local createfile = false
 
 -- Create a new file object. Accepts a variable number of paths, which it will
@@ -97,7 +95,7 @@ function file.read(f)
 		path = f.path
 	end
 
-	local fh = io.open(lua_base_path..f.path, 'r')
+	local fh = io.open(lua_base_path..path, 'r')
 	content = fh:read('*all*')
 	fh:close()
 
@@ -107,6 +105,38 @@ end
 -- Read from file and return lines of the contents in a table.
 function file.readlines(f)
 	return file.read(f):split('\n')
+end
+
+-- Return an iterator over the lines of a file.
+function file.it(f)
+	local path
+	if type(f) == 'string' then
+		if not file.exists(f) then
+			return nil, 'File \''..f..'\' not found, cannot read.'
+		end
+
+		path = f
+	else
+		if f.path == nil then
+			return nil, 'No file path set, cannot write.'
+		end
+
+		if not f:exists() then
+			if createfile then
+				return ''
+			else
+				return nil, 'File \''..f.path..'\' not found, cannot read.'
+			end
+		end
+
+		path = f.path
+	end
+
+	return coroutine.wrap(function()
+		for l in io.lines(lua_base_path..path) do
+			coroutine.yield(l)
+		end
+	end)
 end
 
 -- Write to file. Overwrites everything within the file, if present.
@@ -136,7 +166,7 @@ function file.write(f, content)
 	end
 	
 	if type(content) == 'table' then
-		content = T(content):concat()
+		content = table.concat(content)
 	end
 	
 	local fh = io.open(lua_base_path..path, 'w')
@@ -148,7 +178,7 @@ end
 
 -- Write array to file. Overwrites everything within the file, if present
 function file.writelines(f, lines)
-	return file.write(f, T(lines):concat('\n'))
+	return file.write(f, table.concat(lines, '\n'))
 end
 
 -- Append to file. Sets a newline per default, unless newline is set to false.
@@ -178,17 +208,13 @@ function file.append(f, content, newline)
 	end
 
 	newline = true and (newline ~= false)
+	local sep = newline and '\n' or ''
+	
 	if type(content) == 'table' then
-		if newline then
-			content = T(content):concat('\n')
-		else
-			content = T(content):concat()
-		end
+		content = table.concat(content, sep)
 	end
 
-	if newline then
-		content = '\n'..content
-	end
+	content = sep..content
 	local fh = io.open(lua_base_path..path, 'a')
 	fh:write(content)
 	fh:close()
@@ -198,7 +224,7 @@ end
 
 -- Append an array of lines to file. Sets a newline per default, unless newline is set to false.
 function file.appendlines(f, lines, newline)
-	return file.append(f, lines:concat('\n'), newline)
+	return file.append(f, table.concat(lines, '\n'), newline)
 end
 
 return file
