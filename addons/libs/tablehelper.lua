99ec2f1be9a6887d501ff3a22d995c61086b3fa7
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-05-17 22:19:31 +0200

    tablehelper.lua: More efficient table.concat

--- addons/libs/tablehelper.lua
+++ addons/libs/tablehelper.lua
@@ -526,7 +526,12 @@ _raw.table.concat = table.concat
 
 -- Concatenates all objects of a table. Converts to string, if not already so.
 function table.concat(t, str)
-	return _raw.table.concat(table.map(t, tostring), str)
+	local res = ''
+	for _, val in pairs(t) do
+		res = res..tostring(val)..str
+	end
+	
+	return res
 end
 
 -- Concatenates all elements with a whitespace in between.
