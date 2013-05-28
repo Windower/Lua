ed5e94b28e3b2feb7c6e747ff936f1e6d7fac2fa
Podginator <Podginator@gmail.com>
2013-05-28 23:54:37 +0100

    remove (

--- addons/rolltracker/rolltracker.lua
+++ addons/rolltracker/rolltracker.lua
@@ -177,9 +177,9 @@ function event_action(act)
 								luckyroll = 1
 								add_to_chat(1, '['..#effected_member..'] '..effected_write..string.char(31,1)..' '..symbolnum['implies']..' '..roll_ident[tostring(roller)]..' Roll '..symbolnum['circle'..rollnum]..string.char(31,158)..' (Lucky!)'..string.char(31,13)..' (+'..roll_buff[roll_ident[tostring(roller)]][rollnum]..roll_buff[roll_ident[tostring(roller)]][13]..')'..bustrate)
 							elseif rollnum==12 and #effected_member > 0 then
-								add_to_chat(1, string.char(31,167)..'['..#effected_member..']'..effected_write..' '..symbolnum['implies']..'('..roll_buff[roll_ident[tostring(roller)]][rollnum]..roll_buff[roll_ident[tostring(roller)]][13]..')')
+								add_to_chat(1, string.char(31,167)..'['..#effected_member..']'..effected_write..' '..symbolnum['implies']..' ('..roll_buff[roll_ident[tostring(roller)]][rollnum]..roll_buff[roll_ident[tostring(roller)]][13]..')')
 							else
-								add_to_chat(1, '['..#effected_member..'] '..effected_write..string.char(31,1)..' '..symbolnum['implies']..' '..roll_ident[tostring(roller)]..' Roll ('..symbolnum['circle'..rollnum]..string.char(31,13)..' (+'..roll_buff[roll_ident[tostring(roller)]][rollnum]..roll_buff[roll_ident[tostring(roller)]][13]..')'..bustrate)
+								add_to_chat(1, '['..#effected_member..'] '..effected_write..string.char(31,1)..' '..symbolnum['implies']..' '..roll_ident[tostring(roller)]..' Roll '..symbolnum['circle'..rollnum]..string.char(31,13)..' (+'..roll_buff[roll_ident[tostring(roller)]][rollnum]..roll_buff[roll_ident[tostring(roller)]][13]..')'..bustrate)
 							end
 						end
 					end
