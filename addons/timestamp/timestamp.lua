ee8642b7fb27eed37092d6cdd1e95a36720fc977
Giuliano Riccio <cosmo87rg@gmail.com>
2013-05-29 22:12:06 +0200

    new addon: timestamp

--- addons/timestamp/timestamp.lua
+++ addons/timestamp/timestamp.lua
@@ -1,5 +1,5 @@
 --[[
-timestamp v1.20130528
+timestamp v1.20130529
 
 Copyright (c) 2013, Giuliano Riccio
 All rights reserved.
@@ -28,16 +28,81 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
-local config    = require 'config'
-local timestamp = {}
+require 'colors'
+require 'logger'
+require 'tablehelper'
 
-timestamp.defaults = T{}
-timestamp.defaults.ampm = false
+local config = require 'config'
 
-timestamp.settings = T{}
+_addon = {}
+_addon.name = 'timestamp'
+_addon.version = '1.20130529'
+
+function timezone(separator)
+    local now  = os.time()
+    local h, m = math.modf(os.difftime(now, os.time(os.date('!*t', now))) / 3600)
+
+    if separator == nil or separator == false then
+        return string.format("%+.4d", 100 * h + 60 * m)
+    else
+        return string.format("%+.2d:%.2d", h, 60 * m)
+    end
+end
+
+tz     = timezone()
+tz_sep = timezone(true)
+
+constants = T{
+    ['year']         = '%Y',
+    ['y']            = '%Y',
+    ['year_short']   = '%y',
+    ['month']        = '%m',
+    ['m']            = '%m',
+    ['month_short']  = '%b',
+    ['month_long']   = '%B',
+    ['day']          = '%d',
+    ['d']            = '%d',
+    ['day_short']    = '%a',
+    ['day_long']     = '%A',
+    ['hour']         = '%H',
+    ['h']            = '%H',
+    ['hour24']       = '%H',
+    ['hour12']       = '%I',
+    ['minute']       = '%M',
+    ['min']          = '%M',
+    ['second']       = '%S',
+    ['s']            = '%S',
+    ['sec']          = '%S',
+    ['ampm']         = '%p',
+    ['timezone']     = tz,
+    ['tz']           = tz,
+    ['timezone_sep'] = tz_sep,
+    ['tz_sep']       = tz_sep,
+    ['time']         = '%H:%M:%S',
+    ['date']         = '%Y-%m-%d',
+    ['datetime']     = '%Y:%m:%d %H:%M:%S',
+    ['iso8601']      = '%Y-%m-%dT%H:%M:%S'..tz_sep,
+    ['rfc2822']      = '%a, %d %b %Y %H:%M:%S '..tz,
+    ['rfc822']       = '%a, %d %b %y %H:%M:%S '..tz,
+    ['rfc1036']      = '%a, %d %b %y %H:%M:%S '..tz,
+    ['rfc1123']      = '%a, %d %b %Y %H:%M:%S '..tz,
+    ['rfc3339']      = '%Y-%m-%dT%H:%M:%S'..tz_sep
+}
+
+defaults = {}
+defaults.color  = 508
+defaults.format = '${time}'
+
+settings = {}
+
+function get_string(format)
+	local formatted_string = format:gsub('%${([%l%d_]+)}', function(match) if constants[match] ~= nil then return os.date(constants[match]) else return match end end)
+
+    return formatted_string
+end
 
 function event_load()
-    timestamp.settings = config.load(timestamp.defaults)
+    settings = config.load(defaults)
 
     send_command('alias timestamp lua c timestamp')
 end
@@ -48,15 +113,9 @@ end
 
 function event_incoming_text(original, modified, mode)
     if modified ~= '' and not ((mode == 150 or mode == 151) and (modified:find('\x7f\x31$') ~= nil or modified:find('\x7f\x34$') ~= nil)) then
-        local timeString
-        
-        if timestamp.settings.ampm == false then
-            timeString = os.date('%H:%M:%S')
-        else
-            timeString = os.date('%I:%M:%S '..os.date('%p'):upper())
-        end
-            
-        return '\x1E\xFC['..timeString..']\x1E\x01 '..modified:gsub('\x07', '\x07\x1E\xFC['..timeString..']\x1E\x01 ')
+        local timeString = ('['..get_string(settings.format)..']'):color(settings.color)..' '
+
+        return timeString..modified:gsub('\x07', '\x07'..timeString)
     end
 
     return modified, mode
@@ -64,31 +123,62 @@ end
 
 function event_addon_command(...)
     local args = {...}
-
-    if #args == 0 then
-        add_to_chat(55,'lua:addon:timestamp >> switches between military and ampm modes.')
-        add_to_chat(55,'lua:addon:timestamp >> usage: timestamp ampm [\30\02enabled\30\01]')
-        add_to_chat(55,'lua:addon:timestamp >> positional arguments:')
-        add_to_chat(55,'lua:addon:timestamp >>   enabled    defines the status of ampm mode. "0", "false" or "default" to disable it. "1" or "true" to enable it. if not specified, "true" will be assumed.')
+    
+    if args[1] == nil then
+        send_command('timestamp help')
 
         return
     end
 
-    if args[1] == 'ampm' then
-        if args[2] == '0' or args[2] == 'false' or args[2] == 'default' then
-            timestamp.settings.ampm = false
+    local cmd = table.remove(args, 1):lower()
 
-            timestamp.settings:save('all')
-            add_to_chat(55,'lua:addon:timestamp >> ampm mode has been disabled.')
-        elseif args[2] == '1' or args[2] == 'true' or args[2] == nil then
-            timestamp.settings.ampm = true
+    if cmd == 'help' then
+        log('\x81\xa1 timestamp [<command>] help -- shows the help text.')
+        log('\x81\xa1 timestamp color <color> -- sets the timestamp\'s color.')
+        log('\x81\xa1 timestamp format <format> -- sets the timestamp\'s format.')
 
-            timestamp.settings:save('all')
-            add_to_chat(55,'lua:addon:timestamp >> ampm mode has been enabled.')
+        return
+    elseif cmd == 'format' then
+        if args[1] == nil then
+            error('Please specify the new timestamp\'s format.')
+        elseif args[1] == 'help' then
+            log('Sets the timestamp\'s format.')
+            log('Usage: timestamp format [help|<format>]')
+            log('Positional arguments:')
+            log('\x81\xa1 help: shows the help text.')
+            log('\x81\xa1 <format>: defines the timestamp\'s format. The available constants are:')
+
+            for _, key in ipairs(constants:keyset():sort()) do
+                log('  ${'..key..'}: '..get_string('${'..key..'}'))
+            end
         else
-            add_to_chat(38, 'lua:addon:timestamp >> "'..args[2]..'" is not a valid value for \30\02enabled\30\01.')
+            settings.format = args[1]
+
+            settings:save('all')
+            notice('The new timestamp\'s format has been set ('..get_string(settings.format)..').')
+        end
+    elseif cmd == 'color' then
+        if args[1] == nil then
+            error('Please specify the new color.')
+        elseif args[1] == 'help' then    
+            log('Sets the timestamp\'s color.')
+            log('Usage: timestamp color [help|<color>]')
+            log('Positional arguments:')
+            log('\x81\xa1 help: shows the help text.')
+            log('\x81\xa1 <color>: defines the timestamp\'s color. The value must be between 0 and 511, inclusive.')
+        else
+            local color = tonumber(args[1], 10)
+            
+            if color == nil or color < 0 or color >= 512 then
+                error('Please specify a valid color.')
+            else
+                settings.color = color
+
+                settings:save('all')
+                notice('The new color has been set.')
+            end
         end
     else
-        add_to_chat(38, 'lua:addon:timestamp >> "'..args[1]..'" is not a valid command.')
+        error('"'..cmd..'" is not a valid command.')
     end
 end
