2687c27c3947843e047601ebefb15eb7548cd9ce
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-06-01 16:47:40 +0200

    config.lua: Added proper isolation between config.load calls

--- addons/libs/config.lua
+++ addons/libs/config.lua
@@ -13,10 +13,9 @@ local files = require 'filehelper'
 _libs.filehelper = _libs.filehelper or (files ~= nil)
 
 local config = {}
-local file = files.new()
-local original = T{['global'] = T{}}
-local chars = T{}
-local comments = T{}
+
+-- Map for different config loads.
+local settings_map = T{}
 
 --[[
 	Local functions
@@ -51,63 +50,78 @@ function config.load(filename, confdict, overwrite)
 		end
 	end})
 
+	-- Settings member variables, in separate struct
+	local meta = {}
+	meta.file = files.new()
+	meta.original = T{['global'] = T{}}
+	meta.chars = T{}
+	meta.comments = T{}
+
+	settings_map[confdict] = meta
+
 	-- Load addon config file (Windower/addon/<addonname>/data/settings.xml).
 	local filepath = filename or files.check('data/settings.xml')
 	if filepath == nil then
-		file:set(filename or 'data/settings.xml', true)
-		original['global'] = confdict:copy()
+		meta.file:set(filename or 'data/settings.xml', true)
+		meta.original['global'] = confdict:copy()
 		confdict:save()
+
 		return confdict
 	end
-	file:set(filepath)
+
+	meta.file:set(filepath)
 
 	local err
-	confdict, err = parse(file, confdict, overwrite)
+	confdict, err = parse(confdict, overwrite)
 
 	if err ~= nil then
 		error(err)
 	end
-	
-	collectgarbage()
-	
+
 	return confdict
 end
 
 -- Resolves to the correct parser and calls the respective subroutine, returns the parsed settings table.
-function parse(file, confdict, update)
+function parse(confdict, overwrite)
 	local parsed = T{}
 	local err
-	if file.path:endswith('.json') then
-		parsed = json.read(file)
-	elseif file.path:endswith('.xml') then
-		parsed, err = xml.read(file)
+	meta = settings_map[confdict]
+
+	if meta.file.path:endswith('.json') then
+		parsed = json.read(meta.file)
+
+	elseif meta.file.path:endswith('.xml') then
+		parsed, err = xml.read(meta.file)
+
 		if parsed == nil then
 			if err ~= nil then
 				error(err)
 			else
 				error('XML error: Unkown error.')
 			end
-			return T{}
+			return confdict
 		end
+
 		parsed = settings_table(parsed, confdict)
 	end
-	
+
 	-- Determine all characters found in the settings file.
-	chars = parsed:keyset():filter(-'global')
-	original = T{}
-	
-	if update or confdict:isempty() then
-		for char in (L{'global'}+chars):it() do
-			original[char] = confdict:copy():update(parsed[char], true)
+	meta.chars = parsed:keyset():filter(-'global')
+	meta.original = T{}
+
+	if overwrite or confdict:isempty() then
+		for char in (L{'global'}+meta.chars):it() do
+			meta.original[char] = confdict:copy():update(parsed[char], true)
 		end
+
 		return confdict:update(parsed['global']:update(parsed[get_player()['name']:lower()], true), true)
 	end
-	
+
 	-- Update the global settings with the per-player defined settings, if they exist. Save the parsed value for later comparison.
-	for _, char in ipairs(T{'global'}+chars) do
-		original[char] = merge(confdict:copy(), parsed[char], char)
+	for _, char in ipairs(T{'global'}+meta.chars) do
+		meta.original[char] = merge(confdict:copy(), parsed[char], char)
 	end
-	
+
 	return merge(confdict, parsed['global']:update(parsed[get_player()['name']:lower()], true))
 end
 
@@ -116,17 +130,17 @@ function merge(t, t_merge, path)
 	if t_merge == nil then
 		return t
 	end
-	
+
 	path = (type(path) == 'string' and T{path}) or path
 
 	local oldval
 	local err
-	
+
 	local keys = {}
 	for key in pairs(t) do
 		keys[key:lower()] = key
 	end
-	
+
 	local key
 	for lkey, val in pairs(t_merge) do
 		key = keys[lkey]
@@ -243,7 +257,7 @@ function settings_table(node, confdict, key)
 	
 	for _, child in ipairs(node.children) do
 		if child.type == 'comment' then
-			comments[key] = child.value:trim()
+			meta.comments[key] = child.value:trim()
 		elseif child.type == 'tag' then
 			key = child.name:lower()
 			local childdict
@@ -263,28 +277,30 @@ end
 -- char defaults to get_player()['name']. Set to "all" to apply to all characters.
 function config.save(t, char)
 	char = (char or get_player()['name']):lower()
+	meta = settings_map[t]
+
 	if char == 'all' then
 		char = 'global'
-	elseif not chars:contains(char) then
-		chars:append(char)
-		original[char] = T{}
+	elseif not meta.chars:contains(char) then
+		meta.chars:append(char)
+		meta.original[char] = T{}
 	end
 
-	original[char]:update(t)
-	
+	meta.original[char]:update(t)
+
 	if char == 'global' then
-		original = original:filterkey('global')
+		meta.original = meta.original:filterkey('global')
 	else
-		original[char] = table_diff(original['global'], original[char]) or T{}
-		
-		if original[char]:isempty() then
-			original[char] = nil
-			chars:delete(char)
+		meta.original.global:amend(meta.original[char])
+		meta.original[char] = table_diff(meta.original['global'], meta.original[char]) or T{}
+
+		if meta.original[char]:isempty() then
+			meta.original[char] = nil
+			meta.chars:delete(char)
 		end
 	end
-	
-	local wot = settings_xml(original)
-	file:write(wot)
+
+	meta.file:write(settings_xml(meta))
 end
 
 -- Returns the table containing only elements from t_new that are different from t and not nil.
@@ -314,43 +330,46 @@ function table_diff(t, t_new)
 			end
 		end
 	end
-	
+
 	if res:isempty() then
 		return nil
 	end
-	
+
 	return res
 end
 
 -- Converts a settings table to a XML representation.
-function settings_xml(settings)
+function settings_xml(meta)
 	local str = '<?xml version="1.1" ?>\n'
 	str = str..'<settings>\n'
-	
-	chars = settings:keyset():filter(-functools.equals('global')):sort()
-	for char in (L{'global'}+chars):it() do
-		if char == 'global' and rawget(comments, 'settings') ~= nil then
+	settings = meta.original
+
+	meta.chars = settings:keyset():filter(-'global'):sort()
+	for char in (L{'global'}+meta.chars):it() do
+		if char == 'global' and rawget(meta.comments, 'settings') ~= nil then
 			str = str..'\t<!--\n'
-			local comment_lines = rawget(comments, 'settings'):split('\n')
+			local comment_lines = rawget(meta.comments, 'settings'):split('\n')
 			for comment in comment_lines:it() do
 				str = str..'\t\t'..comment:trim()..'\n'
 			end
+
 			str = str..'\t-->\n'
 		end
+
 		str = str..'\t<'..char..'>\n'
-		str = str..nest_xml(settings[char], 2)
+		str = str..nest_xml(settings[char], meta, 2)
 		str = str..'\t</'..char..'>\n'
 	end
-	
+
 	str = str..'</settings>\n'
 	return str
 end
 
 -- Converts a table to XML without headers using appropriate indentation and comment spacing. Used in settings_xml.
-function nest_xml(t, indentlevel)
+function nest_xml(t, meta, indentlevel)
 	indentlevel = indentlevel or 0
 	local indent = ('\t'):rep(indentlevel)
-	
+
 	local inlines = T{}
 	local fragments = T{}
 	local maxlength = 0		-- For proper comment indenting
@@ -360,16 +379,17 @@ function nest_xml(t, indentlevel)
 		val = rawget(t, key)
 		if type(val) == 'table' and not (class(val) == 'List' or T(val):isarray()) then
 			fragments:append(indent..'<'..key..'>\n')
-			if rawget(comments, key) ~= nil then
-				local c = ('<!-- '..rawget(comments, key):trim()..' -->'):split('\n')
+			if rawget(meta.comments, key) ~= nil then
+				local c = ('<!-- '..rawget(meta.comments, key):trim()..' -->'):split('\n')
 				local pre = ''
 				for cstr in c:it() do
 					fragments:append(indent..pre..cstr:trim()..'\n')
 					pre = '\t '
 				end
 			end
-			fragments:append(nest_xml(val, indentlevel + 1))
+			fragments:append(nest_xml(val, meta, indentlevel + 1))
 			fragments:append(indent..'</'..key..'>\n')
+
 		else
 			if class(val) == 'List' then
 				val = val:format('csv')
@@ -390,25 +410,16 @@ function nest_xml(t, indentlevel)
 			inlines[fragments:length()] = key
 		end
 	end
-	
+
 	for frag_key, key in pairs(inlines) do
-		if rawget(comments, key) ~= nil then
-			fragments[frag_key] = fragments[frag_key]..(' '):rep(maxlength - fragments[frag_key]:trim():length() + 1)..'<!-- '..comments[key]..' -->'
+		if rawget(meta.comments, key) ~= nil then
+			fragments[frag_key] = fragments[frag_key]..(' '):rep(maxlength - fragments[frag_key]:trim():length() + 1)..'<!-- '..meta.comments[key]..' -->'
 		end
-		
+
 		fragments[frag_key] = fragments[frag_key]..'\n'
 	end
-	
-	return fragments:concat()
-end
 
--- Resets all data. Always use when loading within a library.
-function config.reset()
-	config = T(config) or T{}
-	file = files.new()
-	original = T{['global'] = T{}}
-	chars = T{}
-	comments = T{}
+	return fragments:concat()
 end
 
 return config
