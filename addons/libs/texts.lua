e13ef4dec529fd73a405dd4b73143e8a9233662d
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-06-02 18:04:45 +0200

    texts.lua: Adjusted texts.visible function

--- addons/libs/texts.lua
+++ addons/libs/texts.lua
@@ -218,8 +218,13 @@ function texts.hide(t)
 end
 
 -- Returns whether or not the text object is visible.
-function texts.visible(t)
-	return t._settings.visible
+function texts.visible(t, visible)
+	if not visible then
+		return t._settings.visible
+	end
+
+	tb_set_vsibility(t._name, visible)
+	t._settings.visible = visible
 end
 
 -- Sets the text. This will ignore the defined text patterns.
