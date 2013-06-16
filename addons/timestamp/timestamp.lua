6ac4bf16bb2fe6afbc0ee9a108dc7650527a6972
Giuliano Riccio <cosmo87rg@gmail.com>
2013-06-16 16:41:21 +0200

    readded indentation fixes

--- addons/timestamp/timestamp.lua
+++ addons/timestamp/timestamp.lua
@@ -36,8 +36,7 @@ config = require 'config'
 
 _addon = {}
 _addon.name    = 'timestamp'
-_addon.command = 'timestamp'
-_addon.version = '1.3'
+_addon.version = '1.20130616'
 
 function timezone()
     local now  = os.time()
@@ -85,12 +84,11 @@ constants = T{
     ['rfc3339']      = '%Y-%m-%dT%H:%M:%S'..tz_sep
 }
 
+lead_bytes_pattern = '\x1E\x1F\xF7\xEF\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89'
+
 defaults = {}
 defaults.color  = 508
 defaults.format = '[${time}]'
-
--- Lead bytes in multi-byte chars
-lead_bytes = S{0x1E, 0x1F, 0xF7, 0xEF, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89}
 
 settings = {}
 
@@ -98,6 +96,10 @@ function get_string(format)
     local formatted_string = format:gsub('%${([%l%d_]+)}', function(match) if constants[match] ~= nil then return os.date(constants[match]) else return match end end)
 
     return formatted_string
+end
+
+function initialize()
+    settings = config.load(defaults)
 end
 
 function event_load()
@@ -112,46 +114,40 @@ function event_login()
     initialize()
 end
 
-function initialize()
-    settings = config.load(defaults)
-end
-
 function event_unload()
     send_command('unalias timestamp')
 end
 
 function event_incoming_text(original, modified, mode)
-    if modified == '' or mode == 150 or mode == 151 then
-        return
-    end
-
-    local timeString = (os.date((settings.format:gsub('%${([%l%d_]+)}', constants)))):color(settings.color)..' '
-
-    local res = timeString
-    local prev = 0
-    local buffer = ''
-    for c in modified:it() do
-        res = res..buffer..c
-        buffer = ''
-        b = string.byte(c)
-        if (b == 0x0A or b == 0x07) and not (lead_bytes:contains(prev)) then
-            buffer = timeString
+    if modified ~= '' and not modified:find('^[%s]+$') then
+        if mode == 144 then -- 144 works as 150 but the enter prompt are ignored.
+            mode     = 150
+            modified = modified:gsub('\x7f\x31$', '')
         end
-        prev = b
+
+        if mode == 150 then -- 144/150 automatically indents new lines. 151 works the same way but with no indentation. redirect to 151 and manually add the ideographic space.
+            mode     = 151
+            modified = modified:gsub('([^'..lead_bytes_pattern..'])[\x07\n]', '%1\n\x81\x40')
+        end
+
+        if mode ~= 151 then
+            local timeString = ('['..get_string(settings.format)..']'):color(settings.color)..' '
+
+            modified = timeString..modified:gsub('^[\x07\n]+', ''):gsub('([^'..lead_bytes_pattern..'])\n+$', '%1'):gsub('([^'..lead_bytes_pattern..'])[\x07\n]', '%1\n'..timeString)
+        end
     end
 
-    return res, mode
+    return modified, mode
 end
 
 function event_addon_command(...)
-    local cmd = (...) and (...):lower() or 'help'
+    local cmd  = (...) and (...):lower() or 'help'
     local args = {select(2, ...)}
 
     if cmd == 'help' then
         log('\x81\xa1 timestamp [<command>] help -- shows the help text.')
         log('\x81\xa1 timestamp color <color> -- sets the timestamp\'s color.')
         log('\x81\xa1 timestamp format <format> -- sets the timestamp\'s format.')
-
     elseif cmd == 'format' then
         if not args[1] then
             error('Please specify the new timestamp\'s format.')
@@ -169,9 +165,8 @@ function event_addon_command(...)
             settings.format = args[1]
 
             settings:save()
-            log('The new timestamp format has been saved ('..get_string(settings.format)..').')
+            log('The new timestamp\'s format has been saved ('..get_string(settings.format)..').')
         end
-
     elseif cmd == 'color' then
         if not args[1] then
             error('Please specify the new color.')
@@ -190,12 +185,12 @@ function event_addon_command(...)
                 settings.color = color
 
                 settings:save()
-                log('The new timestamp color has been saved ('..color..').')
+                log('The new timestamp\'s color has been saved ('..color..').')
             end
         end
-
     elseif cmd == 'save' then
         settings:save('all')
-
+    else
+        send_command('timestamp help')
     end
 end
