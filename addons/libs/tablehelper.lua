5ff2e00702237bbea96da749a8adf28c42ed07fc
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2013-08-23 07:56:28 +0200

    Libs/Tablehelper: Added depth-check to table.equals

--- addons/libs/tablehelper.lua
+++ addons/libs/tablehelper.lua
@@ -280,18 +280,33 @@ function table.flatten(t, recursive)
 end
 
 -- Returns true if all key-value pairs in t_eq equal all key-value pairs in t.
-function table.equals(t, t_eq)
+function table.equals(t, t_eq, depth)
+    depth = depth or -1
+    if depth == 0 then
+        return t == t_eq
+    end
+    if class(t) ~= class(t_eq) then
+        return false
+    end
+
 	local seen = {}
 
 	for key, val in pairs(t) do
-		if t_eq[key] ~= val then
-			return false
+        local cval = rawget(t_eq, key)
+		if val ~= cval then
+            if type(val) == 'table' and class(val) == class(cval) then
+                if not table.equals(val, cval, depth - 1) then
+                    return false
+                end
+            else
+                return false
+            end
 		end
 		seen[key] = true
 	end
 
 	for key, val in pairs(t_eq) do
-		if seen[key] == nil then
+		if not seen[key] then
 			return false
 		end
 	end
@@ -631,3 +646,16 @@ end
 function table.min(t)
 	return table.reduce(t, math.max)
 end
+
+--[[
+Copyright (c) 2013, Windower
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Windower nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Windower BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
