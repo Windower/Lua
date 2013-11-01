7f6f1d43d2e4616e21e73283b47b616bcbce48d4
Byrth Noth <squarefocus@hotmail.com>
2013-11-01 10:59:15 -0400

    Distance - Minor syntax update

--- addons/distance/distance.lua
+++ addons/distance/distance.lua
@@ -22,9 +22,7 @@ settings = config.load(defaults)
 distance = texts.new(settings)
 
 register_event('prerender', function()
-	local t
-	t = get_mob_by_target('st')
-	if not t then t = get_mob_by_target('t') end
+	local t = get_mob_by_target('st') or get_mob_by_target('t')
     distance:text(t and string.format('%.1f', math.sqrt(t.distance)) or '')
     distance:visible(t ~= nil or true)
 end)
