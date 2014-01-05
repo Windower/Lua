81d9f2341205594902a8b2bcf7572221fc291f02
Byrth Noth <squarefocus@hotmail.com>
2014-01-05 10:04:27 -0500

    GearSwap - Version bump

--- addons/GearSwap/gearswap.lua
+++ addons/GearSwap/gearswap.lua
@@ -25,7 +25,7 @@
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'GearSwap'
-_addon.version = '0.813'
+_addon.version = '0.814'
 _addon.author = 'Byrth'
 _addon.commands = {'gs','gearswap'}
 
