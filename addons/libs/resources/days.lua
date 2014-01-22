49c76322e4aec8583ad516b33b8aa5be9ea2c1e3
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2014-01-22 18:11:50 +0100

    Libs/Resources: Added days

---
+++ addons/libs/resources/days.lua
@@ -0,0 +1,26 @@
+-- Item days
+local days = {}
+
+days[0] = {english = 'Firesday',        element = 0}
+days[1] = {english = 'Earthsday',       element = 3}
+days[2] = {english = 'Watersday',       element = 5}
+days[3] = {english = 'Windsday',        element = 2}
+days[4] = {english = 'Iceday',          element = 1}
+days[5] = {english = 'Lightningsday',   element = 4}
+days[6] = {english = 'Lightsday',       element = 6}
+days[7] = {english = 'Darksday',        element = 7}
+
+return days
+
+--[[
+Copyright (c) 2013, Windower
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Windower nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Windower BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
