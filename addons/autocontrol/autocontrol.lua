11cf62344ff25e7f7f8aa98666019ab719480a5d
Nitrous <nitrous24@gmail.com>
2014-04-30 04:54:41 -0400

    Update autocontrol.lua

--- addons/autocontrol/autocontrol.lua
+++ addons/autocontrol/autocontrol.lua
@@ -27,7 +27,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
 _addon.name = 'autocontrol'
-_addon.version = '1.0'
+_addon.version = '1.01'
 _addon.author = 'Nitrous (Shiva)'
 _addon.commands = {'autocontrol','acon'}
 
