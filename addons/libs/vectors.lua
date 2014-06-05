6f69cff3701451feedac1d6a97b744ffa3481085
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2014-06-05 18:04:12 +0200

    Libs/Vectors: Removed unused functions, added radian conversion and angle calculation

--- addons/libs/vectors.lua
+++ addons/libs/vectors.lua
@@ -134,6 +134,16 @@ end
 
 _meta.V.__sub = vector.subtract
 
+-- Returns the angle described by two vectors (in radians)
+function vector.angle(v1, v2)
+    return ((v1 * v2) / (v1:length() * v2:length())):acos()
+end
+
+-- Returns a 2D vector from a radian value
+function vector.from_radian(r)
+    return V{r:cos(), -r:sin()}
+end
+
 -- Returns the vector in string format: (...)
 function vector.tostring(v)
     local str = '('
@@ -149,26 +159,8 @@ end
 
 _meta.V.__tostring = vector.tostring
 
-function vector.tovstring(v)
-    local str = ''
-    for i, val in ipairs(v) do
-        if i > 1 then
-            str = str..'\n'
-        end
-        str = str..tostring(val)
-    end
-
-    return str
-end
-
-function vector.vprint(v)
-    if log then
-        log(v:tovstring())
-    end
-end
-
 --[[
-Copyright (c) 2013, Windower
+Copyright © 2013-2014, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
