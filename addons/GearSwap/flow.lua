901a05ea4a928ffd720c192cd2d364dfb4bb63ea
Byrth Noth <squarefocus@hotmail.com>
2014-06-29 23:06:12 -0400

    Battlemod lag preventer

--- addons/GearSwap/flow.lua
+++ addons/GearSwap/flow.lua
@@ -403,10 +403,12 @@ end
 windower.register_event('outgoing chunk',function(id,original,modified,injected,blocked)
     if gearswap_disabled then return end
     windower.debug('outgoing chunk '..id)
-    if id == 0x1A and not injected then
-        if original:unpack('H',0xB) == 12 and cued_packet and original:unpack('H',0x9) == player.index then
-            cued_packet = nil
-            return true
+    if id == 0x1A then
+        cued_packet = nil
+        if not injected then
+            if original:unpack('H',0xB) == 12 and cued_packet and original:unpack('H',0x9) == player.index then
+                return true
+            end
         end
     elseif id == 0x100 then
     -- Scrub the equipment array if a valid outgoing job change packet is sent.
