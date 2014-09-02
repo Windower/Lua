5e7bc2e15c06feeb3b6f2f1739c932deb17938b8
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2014-09-02 14:08:18 +0200

    GearSwap: Added select to the user environment

--- addons/GearSwap/refresh.lua
+++ addons/GearSwap/refresh.lua
@@ -104,8 +104,8 @@ function load_user_files(job_id,user_file)
         -- Library functions
         string=string,math=math,table=table,set=set,list=list,T=T,S=S,L=L,pack=pack,
         os=os,texts=texts,type=type,tostring=tostring,tonumber=tonumber,pairs=pairs,
-        ipairs = ipairs, print=print, add_to_chat=add_to_chat_user,unpack=unpack,
-        next=next,lua_base_path=windower.addon_path,empty=empty,file=file,
+        ipairs=ipairs, print=print, add_to_chat=add_to_chat_user,unpack=unpack,next=next,
+        select=select,lua_base_path=windower.addon_path,empty=empty,file=file,
         loadstring=loadstring,assert=assert,error=error,pcall=pcall,io=io,
         
         debug=debug,coroutine=coroutine,setmetatable=setmetatable,getmetatable=getmetatable,
