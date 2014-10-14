7e7ffd6032915bed08d732b00e5c164edafdaf70
Skyrant <sascha.tausend@gmail.com>
2014-10-14 16:09:16 +0200

    Create irc.lua
    This is an irc chat based addon. It works with twitch.tv and should work with most other irc servers. Currently it is not possible to set the chat to default to the irc server but a functionality like /cm i will be added in the future to make it act like a normal Linkshell.
    
    Known issues: The games autotranslate text causes problems when sent to the IRC server.

---
+++ addons/irc/irc.lua
@@ -0,0 +1,195 @@
+--[[
+
+Please edit the ./data/settings.xml to set username and password.
+
+This is a very early version of a IRC based Linkshell chat addon.
+It currently works fine with most IRC servers even those that require authentification.
+It was aminly developed to talk with people on twitch.tv that watch your stream without
+the need to have a second monitor to view the chat.
+
+Suggestions are very welcome.
+Skyrant
+
+]]
+
+socket = require "socket"
+
+_addon.version = '0.1'
+_addon.name = 'irc'
+_addon.author = 'Skyrant, maintainer: Skyrant'
+_addon.commands = {'i','irc'}
+
+require('chat')
+require('logger')
+require('strings')
+require('maths')
+require('tables')
+
+config = require('config')
+settings = config.load(defaults)
+
+ircconnect = function()
+
+	connecttime = os.time()
+
+	serveraddr = settings.server.serveraddr
+	serverport = settings.server.serverport
+
+	if serveraddr and serverport then
+		-- eventually do something here
+	else
+		log("setting default twitch server and port")
+		serveraddr = 'irc.twitch.tv'
+		serverport = '6667'
+	end
+	serverport = tonumber(serverport)
+	server = socket.tcp()
+	status = server:connect(serveraddr, serverport)
+	-- twitch irc servers
+	-- irc.twitch.tv:6667 -- use this for your twitch channel
+	-- server:send(perform)
+	server:send("TWITCHCLIENT 2\r\n")
+	server:send("PASS "..settings.login.password.."\r\n")
+	server:send("NICK "..settings.login.nick.."\r\n")
+	server:send("JOIN "..settings.login.channel.."\r\n")
+	server:settimeout(0.001)
+end
+
+tensecwait = function()
+	waittime = os.time()
+	while os.difftime(os.time(),waittime) < 10 do
+		-- waiting...
+	end
+end
+
+checkstatus = function()
+	if not status then
+		log("Not connected to an IRC server.")
+	else
+		log('You are curently connected to')
+		log('Server : '..settings.server.serveraddr..":"..settings.server.serverport)
+		log('Channel: '..settings.login.channel)
+		--server:send("NAMES\r\n")
+	end
+end
+
+if settings.login.autoconnect == "yes" then
+	ircconnect()
+	if not status then log("Reconnecting...") end
+	while not status do
+		if os.difftime(os.time(),connecttime) < 10 then
+			log("Waiting 10 seconds...")
+			tensecwait()
+		end
+		ircconnect()
+		if not status then
+			log("Connection error. Trying again in 10 seconds...")
+			tensecwait()
+		end
+	end
+end
+
+windower.register_event('time change', function(new, old)
+    if not status then
+    	--do nothing
+    	--log("WARNING: Not connected to a server.")
+    	return
+    else
+		local data, error = server:receive("*l")
+		if data then
+			pingdata = string.match(data, '^PING (.+)')
+			if pingdata then
+				server:send("PONG "..pingdata.."\r\n")
+			end
+			nick,msg = string.match(data, '^:([^ ]+) NOTICE .+ :(.+)')
+			if nick and msg then
+				log("NOTICE from "..nick..":",msg)
+			end
+			nick,msg = string.match(data, '^:[^ ]+ (00.) .+ :(.+)')
+			if nick and msg then
+				log("<> "..nick.." <>:",msg)
+			end
+			nick,msg = string.match(data, '^:[^ ]+ 001 (.+) :(.+)')
+			if nick and msg then
+				mynick = nick
+			end
+			nick,msg = string.match(data, '^:([^!]+).+ JOIN (.+)')
+			if nick and msg and nick == mynick then
+				log("<> You have joined "..msg.." <>")
+			end
+			channel,names = string.match(data, '^:[^ ]+ 353 (.+) :(.+)')
+			if channel and names then
+				log("<> Online Users :"..names)
+				--log(topic)
+			end
+			nick,msg = string.match(data, '^:([^!]+).+ PRIVMSG #.+ :(.+)')
+			if nick and msg then
+				windower.add_to_chat(settings.chat.color,"<"..nick.."> "..msg)
+			end
+		end
+	end
+end)
+
+windower.register_event('addon command', function (...)
+	local args	= T{...}:map(string.lower)
+	if args[1] == nil or args[1] == "help" then
+		print('IRC addon by Skyrant')
+		print('-- //i or //irc to send a message to the irc channel')
+		print('General commands:')
+		print('-- //irc checkstatus :: check connection status.')
+		print('-- //irc connect :: connect to the IRC server.')
+		print('-- //irc disconnect :: disconnect from the irc server.')
+		print('Settings:')
+		print('-- //irc chatcolor [colornumber: 001-256] :: change the chatcolor.')
+		print('-- //irc server [servername] :: set the server to connect to.')
+		print('-- //irc port [portnumber] :: set the irc server port.')
+		print('-- //irc channel [channelname] :: switch to a channel (will part current channel).')
+		print("Don't forget to save your settings:")
+		print('-- //irc save')
+
+	elseif args[1] == "chatcolor" and args[2] ~= nil then
+		settings.chat.color = tonumber(args[2])
+		log("chatcolor set to:"..settings.chat.color)
+
+	elseif args[1] == "checkstatus" and args[2] == nil then
+		checkstatus()
+
+	elseif args[1] == "connect" and args[2] == nil then
+		ircconnect()
+
+	elseif args[1] == "disconnect" and args[2] == nil then
+		server:send("PARTALL\r\n")
+		server:send("QUIT\r\n")
+		log('Disconnected from '..settings.server.servername)
+		status = false
+
+	elseif args[1] == "server" and args[2] ~= nil then
+		settings.server.servername = args[2]
+		log("Server changed to "..settings.server.servername)
+
+	elseif args[1] == "port" and args[2] ~= nil then
+		settings.server.serverport = args[3]
+		log("Port changed to "..settings.server.serverport)
+
+	elseif args[1] == "channel" and args[2] ~= nil then
+		settings.login.channel = args[2]
+		server:send("PARTALL\r\n")
+		server:send("JOIN "..settings.login.channel.."\r\n")
+		log("Channel changed to "..settings.login.channel)
+
+	elseif args[1] == 'save' and args[2] == nil then
+		config.save(settings, 'all')
+		log('Settings saved.')
+
+	else
+		message = table.concat( args, " ", 1 )
+		server:send("PRIVMSG "..settings.login.channel.." :"..message.."\r\n")
+		windower.add_to_chat(settings.chat.color,"<"..settings.login.nick.."> "..message)
+
+	end
+end)
+
+windower.register_event('unload',function ()
+ 	server:send("PARTALL\r\n")
+ 	server:send("QUIT\r\n")
+end)
