d7417c7c13a686eb9bef2b00e65b33dc670448b8
Byrth <squarefocus@hotmail.com>
2014-10-17 14:16:32 -0400

    Translate - Dictionary Update

--- addons/translate/dicts/bgthread_guide.lua
+++ addons/translate/dicts/bgthread_guide.lua
@@ -5,7 +5,8 @@ local dict = {
     {en="Ra'Kaznar Skirmish",ja="カザスカ"},
     {en="Voidwatch",ja="ヴォッチ"},
     {en="Burrows",ja="バローズ"},
-    {en="Normal Difficulty",ja="ふつう"},
+    {en="Normal",ja="ふつう"},
+    {en="Normal",ja="普通"},
     {en="Difficult",ja="むず"},
     {en="Very Difficult",ja="とてもむず"},
     {en="GEO",ja="風"},
@@ -21,11 +22,26 @@ local dict = {
     {en="Ra'Kaznar Skirmish",ja="かざすか"},
     {en="Job doesn't matter",ja="ジョブ不問"},
     {en="Recruiting",ja="募集"},
+    
     {en="Check seacom for jobs",ja="サチコ参照ジョブを添えてテルください"},
+    {en="Search Comment",ja="サチコ"},
+    
     {en="Menace",ja="メナス"},
     {en="Morimar",ja="モリマ"},
     {en="Kamihr",ja="カミール"},
     {en="Marjami",ja="マリアミ"},
+    {en="Rala",ja="ララ"},
+    {en="Yorcia",ja="ヨル"},
+    {en="Foret",ja="水林"},
+    
+    {en="Trial by Fire",ja="火の試練"},
+    {en="Trial by Thunder",ja="雷の試練"},
+    {en="Trial by Earth",ja="土の試練"},
+    {en="Trial by Water",ja="水の試練"},
+    {en="Trial by Ice",ja="氷の試練"},
+    {en="Trial by Wind",ja="風の試練"},
+    {en="Return to Delkfutt's Tower",ja="デルクフの塔 再び"},
+    {en="currently",ja="現在"},
 }
 
 return dict
