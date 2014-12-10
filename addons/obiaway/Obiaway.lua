595d7a6f899c03d408ba58e8691b23968e57f5b3
cantreadcode <veraciti@gmail.com>
2014-12-10 03:21:18 -0500

    update v1.0.3
    - modularized obi get and put into four new functions
    - as a result, three new commands are available:
        (s)ort  -  sorts obi based on day, weather, and buffs lost of gained (same functionality as old 'get' command)
        (g)et [(a)ll|(n)eeded] - get obi
        (p)ut [(a)ll|(n)eeded] - put obi
    - plural of obi is obi
    - improved code per z16's suggestions
    - added new author to copyright information

--- addons/obiaway/Obiaway.lua
+++ addons/obiaway/Obiaway.lua
@@ -1,7 +1,7 @@
 -- 
--- Obiaway v1.0.2
+-- Obiaway v1.0.3
 -- 
--- Copyright (c) 2013, ReaperX
+-- Copyright ©2013-2014, ReaperX, onetime
 -- All rights reserved.
 -- 
 -- Redistribution and use in source and binary forms, with or without
@@ -27,11 +27,11 @@
 -- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 -- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 --
--- Puts elemental obis away when they are no longer needed due
+-- Puts elemental obi away when they are no longer needed due
 -- to day/weather/storm effect change and gets elemental obi based
 -- on same conditions. Uses itemizer addon.
 -- 
--- The advantage of this system compared to moving obis back during 
+-- The advantage of this system compared to moving obi back during 
 -- aftercast is that it avoids excessive inventory movement,
 -- so malfunctions due to inventory filling up completely are
 -- less likely, and timing issues with very fast spells (spell
@@ -40,22 +40,20 @@
 --
 -- Known bugs: 
 --
--- 1. upon activation, it puts only the first unneeded
--- obi away. The function get_needed_obis() tries to put all
+-- 1. Upon activation, it puts only the first unneeded
+-- obi away. The function auto_sort_obi() tries to put all
 -- of them away, but the calls to itemizer are all made instantly
 -- so only the first one is carried out. Usually, only one obi
 -- has to be removed per call, so this is not much of a problem.
 --
--- 2. when weather changes due to zoning, get_obis_in_inventory()
+-- 2. When weather changes due to zoning, get_obi_in_inventory()
 -- is called before inventory has loaded and returns nothing.
 --
 -- 3. Obi is not moved when currently equipped.
--- 
--- To Do: Use a loop to remove and retrieve obi for shorter code
 
 _addon.name = "Obiaway"
-_addon.author = "ReaperX"
-_addon.version = "1.0.2"
+_addon.author = "ReaperX, onetime"
+_addon.version = "1.0.3"
 _addon.commands = {'obi', 'obiaway'}
 
 require('sets')
@@ -64,7 +62,7 @@ config = require('config')
 res = require('resources')
 
 default_settings = {}
-default_settings.notify = 'on'
+default_settings.notify = trues
 default_settings.location = 'sack'
 
 settings = config.load(default_settings)
@@ -77,33 +75,55 @@ end
 windower.register_event('addon command', function()
 
     return function(command, ...)
-        command = command:lower() or 'help'
-        local params = {...}
+        local command = command:lower() or 'help'
+        local params = L{...}:map(string.lower)
 
         if command == 'help' or command == 'h' then
             obi_output("Obiaway v".._addon.version..". Authors: ".._addon.author)
             obi_output("//(obi)away [options]")
             obi_output("   (h)elp :  Displays this help text.")
-            obi_output("   (g)et :  Command for manually getting and removing obis.")
+            obi_output("   (s)ort :  Automatically sorts obi.")
+            obi_output("   (g)et [(a)ll|(n)eeded] :  Gets obi.")
+            obi_output("   (p)ut [(a)ll|(n)eeded] :  Puts obi.")
             obi_output("   (n)otify [on|off] :  Sets obiaway notifcations on or off.")
-            obi_output("   (l)ocation [sack|satchel|case|wardrobe] :  Sets inventory from which to get and put obis.")
+            obi_output("   (l)ocation [sack|satchel|case|wardrobe] :  Sets inventory from which to get and put obi.")
+        elseif command == 'sort' or command == 's' then
+            auto_sort_obi()
+            obi_output("Sorting obi...")
         elseif command == 'get' or command == 'g' then
-            get_needed_obis()
-            obi_output("Sorting obis...")
+            if params[1] == 'all' or params [1] == 'a' then
+                get_all_obi()
+                obi_output('Getting all obi from %s...':format(settings.location))
+            elseif params[1] == 'needed' or params [1] == 'n' then
+                get_needed_obi()
+                obi_output('Getting needed obi from %s...':format(settings.location))
+            else
+                error("Invalid argument. Usage: //obiaway get [all|needed]")
+            end
+        elseif command == 'put' or command == 'p' then
+            if params[1] == 'all' or params [1] == 'a' then
+                put_all_obi()
+                obi_output('Putting all obi into %s...':format(settings.location))
+            elseif params[1] == 'needed' or params [1] == 'n' then
+                put_needed_obi()
+                obi_output('Putting uneeded obi into %s...':format(settings.location))
+            else
+                error("Invalid argument. Usage: //obiaway put [all|needed]")
+            end
         elseif command == 'notify' or command == 'n' then
-            if S{'on'}:contains(params[1]:lower()) then
-                settings.notify = params[1]
+            if params[1] == 'on' then
+                settings.notify = true
                 obi_output("Obiaway notifications are now on")
-            elseif S{'off'}:contains(params[1]:lower()) then
-                settings.notify = params[1]
+            elseif params[1] == 'off' then
+                settings.notify = false
                 obi_output("Obiaway notifications are now off.")
             else
-                error("Invalid argument. Usage: //obiaway [on|off]")
+                error("Invalid argument. Usage: //obiaway notify [on|off]")
             end
         elseif command == 'location' or command == 'l' then
-            if S{'sack','case','satchel','wardrobe'}:contains(params[1]:lower()) then
+            if S{'sack','case','satchel','wardrobe'}:contains(params[1]) then
                 settings.location = params[1]
-                obi_output("Obiaway location set to: "..settings.location)
+                obi_output("Obiaway location set to: %s":format(settings.location))
             else
                 error("Invalid argument. Usage: //obiaway location [sack|satchel|case|wardrobe]")
             end
@@ -113,34 +133,132 @@ windower.register_event('addon command', function()
     end
 end())
 
-function get_obis_in_inventory()
-    obis = {}
-    items = windower.ffxi.get_items()
-    inv = items.inventory
+function get_obi_in_inventory()
+    local obi = {}
+    local items = windower.ffxi.get_items()
+    local inv = items.inventory
     if not inv then return end
-    number = items.max_inventory
+    local number = items.max_inventory
+
     for i=1,number do
         id = inv[i].id
         if ( id>=15435 and id<=15442) then
-            obis["Fire"] = obis["Fire"] or (id == 15435)
-            obis["Ice"] = obis["Ice"] or (id == 15436)
-            obis["Wind"] = obis["Wind"] or (id == 15437)
-            obis["Earth"] = obis["Earth"] or (id == 15438)
-            obis["Lightning"] = obis["Lightning"] or (id == 15439)
-            obis["Water"] = obis["Water"] or (id == 15440)
-            obis["Light"] = obis["Light"] or (id == 15441)
-            obis["Dark"] = obis["Dark"] or (id == 15442)
+            obi["Fire"] = obi["Fire"] or (id == 15435)
+            obi["Ice"] = obi["Ice"] or (id == 15436)
+            obi["Wind"] = obi["Wind"] or (id == 15437)
+            obi["Earth"] = obi["Earth"] or (id == 15438)
+            obi["Lightning"] = obi["Lightning"] or (id == 15439)
+            obi["Water"] = obi["Water"] or (id == 15440)
+            obi["Light"] = obi["Light"] or (id == 15441)
+            obi["Dark"] = obi["Dark"] or (id == 15442)
         end
     end
-    return obis
+
+    return obi
 end
 
-function get_needed_obis()
-    elements = get_all_elements()
-    obis = get_obis_in_inventory()
+function get_needed_obi()
+    local elements = get_all_elements()
+    local obi = get_obi_in_inventory()
+    local str = ''
+    local obi_names = T{
+        Light = 'Korin',
+        Dark = 'Anrin',
+        Fire = 'Karin',
+        Earth = 'Dorin',
+        Water = 'Suirin',
+        Wind = 'Furin',
+        Ice = 'Hyorin',
+        Lightning = 'Rairin'
+    }
+    for name, element in obi_names:it() do
+        if obi[element] and elements[element] > 0 then    
+            str = str..'put "%s Obi" %s;wait .5;':format(name, settings.location)
+            if settings.notify then
+                obi_output('Getting %s Obi from %s.':format(name, settings.location))
+            end
+        end
+    end
+    windower.send_command(str)
+end
+
+function put_unneeded_obi()
+    local elements = get_all_elements()
+    local obi = get_obi_in_inventory()
+    local str = ''
+    local obi_names = T{
+        Light = 'Korin',
+        Dark = 'Anrin',
+        Fire = 'Karin',
+        Earth = 'Dorin',
+        Water = 'Suirin',
+        Wind = 'Furin',
+        Ice = 'Hyorin',
+        Lightning = 'Rairin'
+    }
+    for name, element in obi_names:it() do
+        if obi[element] and elements[element] == 0 then    
+            str = str..'put "%s Obi" %s;wait .5;':format(name, settings.location)
+            if settings.notify then
+                obi_output('Putting %s Obi away into %s.':format(name, settings.location))
+            end
+        end
+    end
+    windower.send_command(str)
+end
+
+function get_all_obi()
+    local elements = get_all_elements()
+    local obi = get_obi_in_inventory()
+    local str = ''
+    local obi_names = T{
+        Light = 'Korin',
+        Dark = 'Anrin',
+        Fire = 'Karin',
+        Earth = 'Dorin',
+        Water = 'Suirin',
+        Wind = 'Furin',
+        Ice = 'Hyorin',
+        Lightning = 'Rairin'
+    }
+    for name, element in obi_names:it() do
+        if not obi[element] then
+            str = str..'get "%s Obi" %s;wait .5;':format(name, settings.location)
+            if settings.notify then
+                obi_output('Getting %s Obi from %s.':format(name, settings.location))
+            end
+        end
+    end
+    windower.send_command(str)
+end
+
+function put_all_obi()
+    local elements = get_all_elements()
+    local obi = get_obi_in_inventory()
+    local str = ''
+    local obi_names = T{
+        Light = 'Korin',
+        Dark = 'Anrin',
+        Fire = 'Karin',
+        Earth = 'Dorin',
+        Water = 'Suirin',
+        Wind = 'Furin',
+        Ice = 'Hyorin',
+        Lightning = 'Rairin'
+    }
+    for name, element in obi_names:it() do
+        if obi[element] then
+            str = str..'put "%s Obi" %s;wait .5;':format(name, settings.location)
+            if settings.notify then
+                obi_output('Putting %s Obi away into %s.':format(name, settings.location))
+            end
+        end
+    end
+    windower.send_command(str)
+end
     
-    areas = {}
-    areas.Cities = S{
+function auto_sort_obi()
+    local cities = S{
         "Ru'Lude Gardens",
         "Upper Jeuno",
         "Lower Jeuno",
@@ -170,156 +288,12 @@ function get_needed_obis()
         "Celennia Memorial Library",
         "Mog Garden"
     }
-    
-    local str = ''
-    if not areas.Cities:contains(res.zones[windower.ffxi.get_info().zone].english) then
-        if obis["Fire"] and elements["Fire"] == 0 then
-            str = str.."put \"Karin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Karin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Earth"] and elements["Earth"] == 0 then
-            str = str.."put \"Dorin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Dorin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Water"] and elements["Water"] == 0 then
-            str = str.."put \"Suirin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Suirin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Wind"] and elements["Wind"] == 0 then
-            str = str.."put \"Furin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Furin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Ice"] and elements["Ice"] == 0 then
-            str = str.."put \"Hyorin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Hyorin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Lightning"] and elements["Lightning"] == 0 then
-            str = str.."put \"Rairin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Rairin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Light"] and elements["Light"] == 0 then    
-            str = str.."put \"Korin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Korin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Dark"] and elements["Dark"] == 0 then    
-            str = str.."put \"Anrin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Anrin Obi away in "..settings.location..".")
-            end
-        end
-        if not obis["Fire"] and elements["Fire"] > 0 then
-            str = str.."get \"Karin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Karin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Earth"] and elements["Earth"] > 0 then
-            str = str.."get \"Dorin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Dorin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Water"] and elements["Water"] > 0 then
-            str = str.."get \"Suirin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Suirin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Wind"] and elements["Wind"] > 0 then
-            str = str.."get \"Furin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Furin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Ice"] and elements["Ice"] > 0 then
-            str = str.."get \"Hyorin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Hyorin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Lightning"] and elements["Lightning"] > 0 then
-            str = str.."get \"Rairin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Rairin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Light"] and elements["Light"] > 0 then    
-            str = str.."get \"Korin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Korin Obi from "..settings.location..".")
-            end
-        end
-        if not obis["Dark"] and elements["Dark"] > 0 then    
-            str = str.."get \"Anrin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Getting Anrin Obi from "..settings.location..".")
-            end
-        end
-        windower.send_command(str)
-    else --in town: put away all obis.
-        if obis["Fire"] then
-            str = str.."put \"Karin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Karin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Earth"] then
-            str = str.."put \"Dorin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Dorin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Water"] then
-            str = str.."put \"Suirin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Suirin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Wind"] then
-            str = str.."put \"Furin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Furin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Ice"] then
-            str = str.."put \"Hyorin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Hyorin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Lightning"] then
-            str = str.."put \"Rairin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Rairin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Light"] then    
-            str = str.."put \"Korin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Korin Obi away in "..settings.location..".")
-            end
-        end
-        if obis["Dark"] then    
-            str = str.."put \"Anrin Obi\" "..settings.location..";wait .5;"
-            if settings.notify == 'on' then
-                obi_output("Putting Anrin Obi away in "..settings.location..".")
-            end
-        end
-        windower.send_command(str)
+
+    if not cities:contains(res.zones[windower.ffxi.get_info().zone].english) then
+        put_unneeded_obi()
+        get_needed_obi()
+    else  -- in town. put away all obi.
+        put_all_obi()
     end
 end
 
@@ -332,7 +306,7 @@ end
 
 function get_all_elements()
 
-    elements = {}           
+    local elements = {}           
     elements["Fire"] = 0
     elements["Earth"] = 0
     elements["Water"] = 0
@@ -343,13 +317,13 @@ function get_all_elements()
     elements["Dark"] = 0
     elements["None"] = 0
 
-    info = windower.ffxi.get_info()
+    local info = windower.ffxi.get_info()
 
     local day_element = res.elements[res.days[info.day].element].english
     elements[day_element] = elements[day_element] + 1
     local weather_element = res.elements[res.weather[info.weather].element].english
     elements[weather_element] = elements[weather_element] + 1
-    buffs = windower.ffxi.get_player().buffs
+    local buffs = windower.ffxi.get_player().buffs
 
     if inTable(buffs, 178) then
       elements["Fire"] = elements["Fire"] + 1
@@ -371,6 +345,6 @@ function get_all_elements()
     return elements
 end
 
-windower.register_event('gain buff', get_needed_obis:cond(function(id) return id >= 178 and id <= 185 end))
-windower.register_event('lose buff', get_needed_obis:cond(function(id) return id >= 178 and id <= 185 end))
-windower.register_event('day change', 'weather change', get_needed_obis)
+windower.register_event('gain buff', auto_sort_obi:cond(function(id) return id >= 178 and id <= 185 end))
+windower.register_event('lose buff', auto_sort_obi:cond(function(id) return id >= 178 and id <= 185 end))
+windower.register_event('day change', 'weather change', auto_sort_obi)
