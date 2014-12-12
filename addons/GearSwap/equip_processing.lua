5c57ddd2b428f1f9084382ff168771f505888b5b
Byrth Noth <squarefocus@hotmail.com>
2014-12-12 17:36:22 -0500

    GearSwap - Superior Filter (v0.897)

--- addons/GearSwap/equip_processing.lua
+++ addons/GearSwap/equip_processing.lua
@@ -40,7 +40,8 @@ function check_wearable(item_id)
     elseif not res.items[item_id].jobs then -- Make sure item can be equipped by specific jobs (unlike pearlsacks).
         --debug_mode_chat('GearSwap (Debug Mode): Item '..(res.items[item_id][language] or item_id)..' does not have a jobs field in the resources.')
     else
-        return (res.items[item_id].jobs[player.main_job_id]) and (res.items[item_id].level<=player.jobs[res.jobs[player.main_job_id].ens]) and (res.items[item_id].races[player.race_id])
+        return (res.items[item_id].jobs[player.main_job_id]) and (res.items[item_id].level<=player.jobs[res.jobs[player.main_job_id].ens]) and (res.items[item_id].races[player.race_id]) and
+            ((player.superior_level or 0) >= (res.items[items_id].superior_level or 0))
     end
     return false
 end
