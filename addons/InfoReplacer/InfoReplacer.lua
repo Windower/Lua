cc2d024473ce5370fb70efb993da16279fa3a111
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2015-01-10 10:28:56 +0100

    InfoReplacer: Added custom variables, refactoring, closes Windower/Issues#660

--- addons/InfoReplacer/InfoReplacer.lua
+++ addons/InfoReplacer/InfoReplacer.lua
@@ -1,4 +1,4 @@
--- Copyright (c) 2014, Cairthenn
+-- Copyright © 2014-2015, Cairthenn
 -- All rights reserved.
 
 -- Redistribution and use in source and binary forms, with or without
@@ -26,32 +26,80 @@
 
 _addon.author = 'Cairthenn'
 _addon.name = 'InfoReplacer'
-_addon.version = '1.0'
+_addon.version = '2.0'
 _addon.command = 'inforeplacer'
 
 require('tables')
 require('strings')
 require('logger')
-res = require('resources')
 
 replace = require('reps')
-items = res.items
+raw = {}
+custom = T{}
+fns = {}
 
-windower.register_event('addon command', function(...)
-	local available = ''
-	for k in pairs(replace) do
-		available = available..', '..k
-	end
-	log('Available replacement variable list (prefix with %): \n'..available:sub(3))
+windower.register_event('addon command', function(command, ...)
+    command = command and command:lower() or 'help'
+    local args = {...}
+
+    if command == 'help' or command == 'h' then
+        print(_addon.name .. 'v.' .. _addon.version)
+        print('    \\cs(51, 153, 255)s\\cs(153, 204, 255)et <name> <value>\\cr - Defines a custom replacement')
+        print('    \\cs(153, 204, 255)set\\cs(51, 153, 255)e\\cs(153, 204, 255)val <name> <code>\\cr - Defines a custom replacement as Lua code')
+        print('    \\cs(51, 153, 255)u\\cs(153, 204, 255)nset <name>\\cr - Removes a previously defined custom replacement')
+        print('    \\cs(51, 153, 255)l\\cs(153, 204, 255)ist\\cr - Lists all custom replacements')
+
+    elseif command == 'list' or command == 'l' then
+        if not custom:empty() then
+            log('Available custom replacement variable list:')
+            for key in custom:keyset():sort():it() do
+                local value = raw[key]
+                if fns[key] then
+                    value = '%s ? %s':format(value, custom[key]())
+                end
+                log('    #' .. key, value)
+            end
+        else
+            log('No custom replacements defined. For default replacements view ' .. windower.addon_path .. 'reps.lua')
+        end
+
+    elseif command == 'set' or command == 's' then
+        if #args < 2 then
+            error('Incorrect syntax. The "set" syntax is as follows: //inforeplacer set <name> <value>')
+            return
+        end
+
+        custom[args[1]] = args[2]:fn()
+        raw[args[1]] = args[2]
+        fns[args[1]] = nil
+
+    elseif command == 'unset' or command == 'u' then
+        if #args < 1 then
+            error('Incorrect syntax. The "unset" syntax is as follows: //inforeplacer unset <name>')
+            return
+        end
+
+        custom[args[1]] = nil
+        raw[args[1]] = nil
+        fns[args[1]] = nil
+
+    elseif command == 'seteval' or command == 'e' then
+        if #args < 2 then
+            error('Incorrect syntax. The "seteval" syntax is as follows: //inforeplacer seteval <name> <value>')
+            return
+        end
+
+        custom[args[1]] = assert(loadstring('return ' .. args[2]))
+        raw[args[1]] = args[2]
+        fns[args[1]] = true
+
+    end
 end)
 
 windower.register_event('outgoing text', function(original,modified,blocked)
-    local t_blocks = modified:psplit('%%[%w_]+',false,true)
-	for k,v in pairs(t_blocks) do
-        local _lower = string.lower(v)
-		if v:sub(1,1) == '%' and replace[_lower:sub(2)] then
-			t_blocks[k] = replace[_lower:sub(2)]() or "None"
-		end
-	end
-	return blocked or t_blocks:concat('')
+    return modified:gsub('%%[^ ]+', function(match)
+        local token = match:sub(2)
+        local fn = custom[token] or replace[token]
+        return fn and fn() or match
+    end)
 end)
