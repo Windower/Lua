a679c5bbf34fe657404ecc449932c77edf096e65
MilkMansKid <wastedfx@gmail.com>
2015-01-17 06:04:12 -0500

    Setting Commands

--- addons/ConsoleBG/ConsoleBG.lua
+++ addons/ConsoleBG/ConsoleBG.lua
@@ -1,9 +1,10 @@
 _addon.name = 'ConsoleBG'
 _addon.author = 'StarHawk'
-_addon.version = '0.9.0.0'
+_addon.version = '0.9.0.1'
 _addon.command = 'consolebg'
 
 config = require('config')
+require('logger')
 
 defaults = {}
 defaults.bg = {}
@@ -28,12 +29,95 @@ config.register(settings, function(settings)
     windower.prim.set_size('ConsoleBG', settings.extents.x, settings.extents.y)
 end)
 
+windower.register_event('addon command', function(command1, command2, command3)
+
+    command1 = command1 and command1:lower() or 'help'
+    command2 = command2 and command2:lower()
+    command3 = command3 and command3:lower() or nil
+
+	if command1 == 'pos' then
+        if not (command2 == 'x' or command2 == 'y') then
+            error('Please specify x or y.')
+		elseif not command3 then
+			error('Please specify a value.')
+			return
+        end
+
+		if command2 == 'x' then
+        settings.pos.x = math.floor(command3)
+        log('Position X set to %s':format(settings.pos.x))
+		config.save(settings)
+		elseif command2 == 'y' then
+		settings.pos.y = math.floor(command3)
+        log('Position Y set to %s':format(settings.pos.y))
+		config.save(settings)
+		end
+
+	elseif command1 == 'bg' then
+	local bgvalue = tonumber(command3)
+        if not (command2 == 'alpha' or command2 == 'red' or command2 == 'green' or command2 == 'blue') then
+            error('Please specify alpha, red, green or blue.')
+			return
+		elseif (not bgvalue) or ((0 > bgvalue) or (bgvalue > 255)) then
+			error('Please specify a value (0-255).')
+			return
+        end
+
+		if command2 == 'alpha' then
+        settings.bg.alpha = math.floor(command3)
+        log('Alpha set to %s':format(settings.bg.alpha))
+		config.save(settings)
+		config.reload(settings)
+		elseif command2 == 'red' then
+        settings.bg.red = math.floor(command3)
+        log('Red set to %s':format(settings.bg.red))
+		config.save(settings)
+		config.reload(settings)
+		elseif command2 == 'green' then
+		settings.bg.green = math.floor(command3)
+        log('Green set to %s':format(settings.bg.green))
+		config.save(settings)
+		config.reload(settings)
+		elseif command2 == 'blue' then
+		settings.bg.blue = math.floor(command3)
+        log('Blue set to %s':format(settings.bg.blue))
+		config.save(settings)
+		config.reload(settings)
+		end
+	elseif command1 == 'extent' then
+        if not (command2 == 'x' or command2 == 'y') then
+            error('Please specify x or y.')
+		elseif not command3 then
+			error('Please specify a value.')
+			return
+        end
+
+		if command2 == 'x' then
+        settings.extents.x = math.floor(command3)
+        log('Extent X set to %s':format(settings.extents.x))
+		config.save(settings)
+		config.reload(settings)
+		elseif command2 == 'y' then
+		settings.extents.y = math.floor(command3)
+        log('Extent Y set to %s':format(settings.extents.y))
+		config.save(settings)
+		config.reload(settings)
+		end
+	
+	elseif command1 == 'help' then
+        print('%s v%s':format(_addon.name, _addon.version))
+        print('    \\cs(255,255,255)bg alpha|blue|green|red <value>\\cr - Changes background color/transparency')
+        print('    \\cs(255,255,255)pos x|y <value>\\cr - Set anchor points')
+        print('    \\cs(255,255,255)extent x|y <value>\\cr - Set the width/height')
+    end
+end)
+
 windower.register_event('prerender', function()
     windower.prim.set_visibility('ConsoleBG', windower.console.visible())
 end)
 
 --[[
-Copyright © 2015, Windower
+Copyright Â© 2015, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
