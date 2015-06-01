0279daa7ab5cc5975f29da3ab9068e57392333f7
Morath <curtiswjguy@gmail.com>
2015-06-01 11:26:30 -0500

    Added dragging back in, Functions added images.width, images.height images.get_extents, images.draggable

--- addons/libs/images.lua
+++ addons/libs/images.lua
@@ -5,7 +5,7 @@
 local images = {}
 local meta = {}
 
-windower.prim.saved_images = {}
+saved_images = {}
 local dragged
 
 local events = {
@@ -19,7 +19,7 @@ local events = {
     scroll_down = true,
     hover = true,
     drag = true,
-    right_drag = true,
+    right_drag = true
 }
 
 _libs = _libs or {}
@@ -55,18 +55,22 @@ default_settings.color.alpha = 255
 default_settings.color.red = 255
 default_settings.color.green = 255
 default_settings.color.blue = 255
+default_settings.size = {}
+default_settings.size.width = 0
+default_settings.size.height = 0
 default_settings.texture = {}
 default_settings.texture.path = ''
 default_settings.texture.fit = true
 default_settings.repeatable = {}
 default_settings.repeatable.x = 1
 default_settings.repeatable.y = 1
+default_settings.draggable = true
 
 math.randomseed(os.clock())
 
 local amend
-amend = function(settings, text)
-    for key, val in pairs(text) do
+amend = function(settings, image)
+    for key, val in pairs(image) do
         local sval = settings[key]
         if sval == nil then
             settings[key] = val
@@ -95,9 +99,11 @@ local apply_settings = function(_, t, settings)
     images.visible(t, meta[t].status.visible)
     images.alpha(t, settings.color.alpha)
     images.color(t, settings.color.red, settings.color.green, settings.color.blue)
+    images.size(t, settings.size.width, settings.size.height)
     images.path(t, settings.texture.path)
     images.fit(t, settings.texture.fit)
     images.repeat_xy(t, settings.repeatable.x, settings.repeatable.y)
+    images.draggable(t, settings.draggable)
 
     call_events(t, 'reload')
 end
@@ -112,9 +118,9 @@ function images.new(str, settings, root_settings)
         root_settings = nil
     else
         root_settings =
-            root_settings and class(root_settings) == 'Settings' and
+            root_settings and class(root_settings) == 'settings' and
                 root_settings
-            or settings and class(settings) == 'Settings' and
+            or settings and class(settings) == 'settings' and
                 settings
             or
                 nil
@@ -123,7 +129,7 @@ function images.new(str, settings, root_settings)
     t = {}
     local m = {}
     meta[t] = m
-    m.name = (_addon and _addon.name or 'image') .. '_gensym_' .. tostring(t):sub(8) .. '_%.8X':format(16^8 * math.random()):sub(3)
+    m.name = (_addon and _addon.name or 'image') .. '_gensym_' .. tostring(t):sub(8) .. '_%.8x':format(16^8 * math.random()):sub(3)
     m.settings = settings or {}
     m.status = m.status or {visible = false, image = {}}
     m.root_settings = root_settings
@@ -151,7 +157,25 @@ function images.new(str, settings, root_settings)
         apply_settings(_, t, settings)
     end
 
+    -- Cache for deletion
+    table.insert(saved_images, 1, t)
+
     return setmetatable(t, _meta.Image)
+end
+
+function images.update(t, attr)
+    attr = attr or {}
+    local m = meta[t]
+
+    -- Add possibly new keys
+    for key, value in pairs(attr) do
+        m.keys[key] = true
+    end
+
+    -- Update all image segments
+    for key in pairs(m.keys) do
+        set_value(t, key, attr[key] == nil and m.values[key] or attr[key])
+    end
 end
 
 function images.clear(t)
@@ -176,6 +200,17 @@ function images.hide(t)
     meta[t].status.visible = false
 end
 
+-- Returns whether or not the image object is visible
+function images.visible(t, visible)
+    local m = meta[t]
+    if visible == nil then
+        return m.status.visible
+    end
+
+    windower.prim.set_visibility(m.name, visible)
+    m.status.visible = visible
+end
+
 --[[
     The following methods all either set the respective values or return them, if no arguments to set them are provided.
 ]]
@@ -183,120 +218,202 @@ end
 function images.pos(t, x, y)
     local m = meta[t]
     if not x then
-        return m.settings.Pos.X, m.settings.Pos.Y
+        return m.settings.pos.x, m.settings.pos.y
     end
 
-    local settings = windower.get_windower_settings()
     windower.prim.set_position(m.name, x, y)
-    m.settings.Pos.X = x
-    m.settings.Pos.Y = y
+    m.settings.pos.x = x
+    m.settings.pos.y = y
 end
 
 function images.pos_x(t, x)
     if not x then
-        return meta[t].settings.Pos.X
+        return meta[t].settings.pos.x
     end
 
-    t:pos(x, meta[t].settings.Pos.Y)
+    t:pos(x, meta[t].settings.pos.y)
 end
 
 function images.pos_y(t, y)
     if not y then
-        return meta[t].settings.Pos.Y
+        return meta[t].settings.pos.y
     end
 
-    t:pos(meta[t].settings.Pos.X, Y)
+    t:pos(meta[t].settings.pos.x, y)
 end
 
 function images.size(t, width, height)
     local m = meta[t]
     if not width then
-        return m.settings.Size.Width and m.settings.Size.Height
+        return m.settings.size.width, m.settings.size.height
     end
 
-    windower.prim.set_size(meta[t].name, width, height)
-    m.settings.Size.Width = width
-    m.settings.Size.Height = height
+    windower.prim.set_size(m.name, width, height)
+    m.settings.size.width = width
+    m.settings.size.height = height
+end
+
+function images.width(t, width)
+    if not width then
+        return meta[t].settings.size.width
+    end
+
+    t:size(width, meta[t].settings.size.height)
+end
+
+function images.height(t, height)
+    if not height then
+        return meta[t].settings.size.height
+    end
+
+    t:size(meta[t].settings.size.width, height)
 end
 
 function images.path(t, path)
     if not path then
-        return meta[t].settings.Texture.Path
+        return meta[t].settings.texture.path
     end
 
     windower.prim.set_texture(meta[t].name, path)
-    meta[t].settings.Texture.Path = path
+    meta[t].settings.texture.path = path
 end
 
 function images.fit(t, fit)
     if not fit then
-        return meta[t].settings.Texture.Fit
+        return meta[t].settings.texture.fit
     end
 
     windower.prim.set_fit_to_texture(meta[t].name, fit)
-    meta[t].settings.Texture.Fit = fit
+    meta[t].settings.texture.fit = fit
 end
 
 function images.repeat_xy(t, x, y)
+    local m = meta[t]
     if not x then
-        return meta[t].settings.Repeatable.X
+        return m.settings.repeatable.x, m.settings.repeatable.y
     end
 
-    windower.prim.set_repeat(meta[t].name, x, y)
-    meta[t].settings.Repeatable.X = x
-    meta[t].settings.Repeatable.Y = y
+    windower.prim.set_repeat(m.name, x, y)
+    m.settings.repeatable.x = x
+    m.settings.repeatable.y = y
+end
+
+function images.draggable(t, drag)
+    if not drag then
+        return meta[t].settings.draggable
+    end
+
+    meta[t].settings.draggable = drag
 end
 
 function images.color(t, red, green, blue)
+    local m = meta[t]
     if not red then
-        return meta[t].settings.Color.Red, meta[t].settings.Color.Green, meta[t].settings.Color.Blue
+        return m.settings.color.red, m.settings.color.green, m.settings.color.blue
     end
 
-    windower.prim.set_color(meta[t].name, meta[t].settings.Color.Alpha, red, green, blue)
-    meta[t].settings.Color.Red = red
-    meta[t].settings.Color.Green = green
-    meta[t].settings.Color.Blue = blue
+    windower.prim.set_color(m.name, m.settings.color.alpha, red, green, blue)
+    m.settings.color.red = red
+    m.settings.color.green = green
+    m.settings.color.blue = blue
 end
 
 function images.alpha(t, alpha)
+    local m = meta[t]
     if not alpha then
-        return meta[t].settings.Color.Alpha
+        return m.settings.color.alpha
     end
 
-    windower.prim.set_color(meta[t].name, alpha, meta[t].settings.Color.Red, meta[t].settings.Color.Green, meta[t].settings.Color.Blue)
-    meta[t].settings.Color.Alpha = alpha
+    windower.prim.set_color(m.name, alpha, m.settings.color.red, m.settings.color.green, m.settings.color.blue)
+    m.settings.color.alpha = alpha
 end
 
--- Sets/returns text transparency. Based on percentage values, with 1 being fully transparent, while 0 is fully opaque.
+-- Sets/returns image transparency. Based on percentage values, with 1 being fully transparent, while 0 is fully opaque.
 function images.transparency(t, alpha)
+    local m = meta[t]
     if not alpha then
-        return 1 - meta[t].settings.Color.Alpha/255
+        return 1 - m.settings.color.alpha/255
     end
 
     alpha = math.floor(255*(1-alpha))
-    windower.prim.set_color(meta[t].name, alpha, meta[t].settings.Color.Red, meta[t].settings.Color.Green, meta[t].settings.Color.Blue)
-    meta[t].settings.Color.Alpha = alpha
+    windower.prim.set_color(m.name, alpha, m.settings.color.red, m.settings.color.green, m.settings.color.blue)
+    m.settings.color.alpha = alpha
 end
 
-function images.visible(t, visible)
-    if visible == nil then
-        return meta[t].settings.Visible
+-- Returns true if the coordinates are currently over the image object
+function images.hover(t, x, y)
+    if not t:visible() then
+        return false
     end
 
-    windower.prim.set_visibility(meta[t].name, visible)
-    meta[t].settings.Visible = visible
+    local pos_x, pos_y = t:pos()
+    local off_x, off_y = t:get_extents()
+    
+    -- print(pos_x, pos_y, off_x, off_y)
+
+    return (pos_x <= x and x <= pos_x + off_x
+        or pos_x >= x and x >= pos_x + off_x)
+    and (pos_y <= y and y <= pos_y + off_y
+        or pos_y >= y and y >= pos_y + off_y)
 end
 
 function images.destroy(t)
-    for i, t_needle in ipairs(windower.prim.saved_images) do
+    for i, t_needle in ipairs(saved_images) do
         if t == t_needle then
-            table.remove(windower.prim.saved_images, i)
+            table.remove(saved_images, i)
             break
         end
     end
     windower.prim.delete(meta[t].name)
     meta[t] = nil
 end
+
+function images.get_extents(t)
+    local m = meta[t]
+    
+    local ext_x = m.settings.pos.x + m.settings.size.width
+    local ext_y = m.settings.pos.y + m.settings.size.height
+
+    return ext_x, ext_y
+end
+
+-- Handle drag and drop
+windower.register_event('mouse', function(type, x, y, delta, blocked)
+    if blocked then
+        return
+    end
+
+    -- Mouse drag
+    if type == 0 then
+        if dragged then
+            dragged.image:pos(x - dragged.x, y - dragged.y)
+            return true
+        end
+
+    -- Mouse left click
+    elseif type == 1 then
+        for _, t in pairs(saved_images) do
+            local m = meta[t]
+            if m.settings.draggable and t:hover(x, y) then
+                local pos_x, pos_y = t:pos()
+                dragged = {image = t, x = x - pos_x, y = y - pos_y}
+                return true
+            end
+        end
+
+    -- Mouse left release
+    elseif type == 2 then
+        if dragged then
+            if meta[dragged.image].root_settings then
+                config.save(meta[dragged.image].root_settings)
+            end
+            dragged = nil
+            return true
+        end
+    end
+
+    return false
+end)
 
 -- Can define functions to execute every time the settings are reloaded
 function images.register_event(t, key, fn)
@@ -331,7 +448,7 @@ end
 return images
 
 --[[
-Copyright © 2013-2015, Windower
+Copyright © 2015, Windower
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
