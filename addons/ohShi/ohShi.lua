243c2430f7ce1a12d16a8cf8482fe8eaa41f9f3c
Nitrous <nitrous24@gmail.com>
2015-09-19 17:04:43 -0400

    Update ohShi.lua
    incremented version

--- addons/ohShi/ohShi.lua
+++ addons/ohShi/ohShi.lua
@@ -28,7 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 _addon.name = 'OhShi'
-_addon.version = '2.54'
+_addon.version = '2.55'
 _addon.author = 'Nitrous (Shiva)'
 _addon.command = 'ohshi'
 
