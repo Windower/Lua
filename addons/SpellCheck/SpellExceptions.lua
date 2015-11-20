fa5a82009397b773e53686ab5dd18a8ffe57f947
Damien Dennehy <damien@dennehy.me>
2015-11-20 00:45:34 +0000

    Added filtering by spell level and by an exception list

---
+++ addons/SpellCheck/SpellExceptions.lua
@@ -0,0 +1,10 @@
+spell_exceptions = {
+    [31] = {id=31,en="Banish IV",ja="バニシュIV"},
+    [40] = {id=40,en="Banishga III",ja="バニシュガIII"},
+    [34] = {id=34,en="Diaga II",ja="ディアガII"},
+    [35] = {id=35,en="Diaga III",ja="ディアガIII"},
+    [358] = {id=358,en="Hastega",ja="ヘイスガ"},
+    [356] = {id=356,en="Paralyga",ja="パライガ"},
+    [359] = {id=359,en="Silencega",ja="サイレガ"},
+	[357] = {id=357,en="Slowga",ja="スロウガ"}
+}
