dfdbbf84d2cdeb247c2debc40b82f82d504576a4
Byrth Noth <squarefocus@hotmail.com>
2015-12-14 18:29:58 -0500

    Remember . . . remember . . . . . remember

--- addons/remember/remember.lua
+++ addons/remember/remember.lua
@@ -30,7 +30,7 @@
 _addon.name = 'Remember'
 _addon.author = 'Byrth'
 _addon.command = 'remember'
-_addon.version = '1.0.2.0'
+_addon.version = '1.0.3.0'
 
 packets = require('packets')
 
@@ -60,11 +60,12 @@ windower.register_event('incoming chunk',function(id,org,mod,inj,blk)
                 -- If the entry is now valid, move it to the valid entries table and reset it in the other tables
                 valid_indices[i] = distance({X=character.x,Y=character.y,Z=character.z},player)
                 indices_under_investigation[i] = nil
-            elseif v~=nil and os.clock()-v > 5 then
-                -- If you asked for an update more than 5 seconds ago and haven't gotten one yet, try again
+                novel_indices[i] = nil
+            elseif v~=nil and os.clock()-v > 4.5 then
+                -- If you asked for an update more than 4.5 seconds ago and haven't gotten one yet, try again
                 indices_under_investigation[i] = nil
             elseif v~= nil then
-                -- Asked for an update 5 or fewer seconds ago, so don't ask again.
+                -- Asked for an update 4.5 or fewer seconds ago, so don't ask again.
                 novel_indices[i] = nil
             end
         end
@@ -72,17 +73,17 @@ windower.register_event('incoming chunk',function(id,org,mod,inj,blk)
         -- After clearing out the ones that finally have proper information and those that you're waiting on, you're left with the novel ones that you haven't attempted to contact anyone about yet
         for i,v in pairs(novel_indices) do
             local character = windower.ffxi.get_mob_by_index(i)
-            if v~= nil and (not character or character.id == 0) then
+            if v~= nil and (not character or character.id == 0) and ((seq_id<2 and (seq_id + 256*256)) or seq_id) >= v+2 then
                 -- We've waited a packet cycle since the first occurance of the index and real information hasn't shown up yet, so request another packet
                 print('Sent out an information request for index',i)
                 windower.packets.inject_outgoing(0x016,string.char(0x016,4,0,0,i%256,math.floor(i/256),0,0))
                 indices_under_investigation[i] = os.clock()
-            elseif v ~= nil then
+            elseif v~=nil and character and character.id ~= 0 then
                 -- Novel index, successfully updated through normal methods.
                 valid_indices[i] = distance({X=character.x,Y=character.y,Z=character.z},player)
                 indices_under_investigation[i] = nil
+                novel_indices[i] = nil
             end
-            novel_indices[i] = nil -- Reset it in anticipation of a new packet
         end
         
         last_novel = nil
@@ -100,7 +101,7 @@ windower.register_event('incoming chunk',function(id,org,mod,inj,blk)
             end
         elseif distance(packet,player) < 50 then
             -- The monster was not in range at its last position update or has never loaded before
-            novel_indices[packet['Index']] = seq_id
+            novel_indices[packet['Index']] = novel_indices[packet['Index']] or seq_id
             last_novel = seq_id
         end
     end
