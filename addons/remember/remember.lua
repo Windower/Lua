b0279b71c5e68b0f5cb22d19ef2a8dd21ddb0264
Byrth Noth <squarefocus@hotmail.com>
2015-12-14 17:56:06 -0500

    Remember v1.0.1.0
    Should help cases where it might consider spamming. Downside is it may
    no longer work. Cannot test right now.

--- addons/remember/remember.lua
+++ addons/remember/remember.lua
@@ -27,15 +27,16 @@
 
 -- Deals with refreshing player information and loading user settings --
 
-_addon.name = 'PacketViewer'
+_addon.name = 'Remember'
 _addon.author = 'Byrth'
 _addon.command = 'remember'
-_addon.version = '1.0.0.0'
+_addon.version = '1.0.1.0'
 
 packets = require('packets')
 
 valid_indices = {}
 novel_indices = {}
+indices_under_investigation = {}
 last_novel = nil
 
 function distance(packet,player)
@@ -45,6 +46,7 @@ end
 windower.register_event('zone change',function()
     valid_indices = {}
     novel_indices = {}
+    indices_under_investigation = {}
     last_novel = nil
 end)
 
@@ -52,17 +54,34 @@ windower.register_event('incoming chunk',function(id,org,mod,inj,blk)
     local seq_id = org:byte(4)+org:byte(3)*256
     if last_novel and seq_id ~= last_novel then
         local player = windower.ffxi.get_mob_by_target('<me>')
+        for i,v in pairs(indices_under_investigation) do
+            local character = windower.ffxi.get_mob_by_index(i)
+            if v~=nil and character and character.id ~= 0 then
+                -- If the entry is now valid, move it to the valid entries table and reset it in the other tables
+                valid_indices[i] = distance({X=character.x,Y=character.y,Z=character.z},player)
+                indices_under_investigation[i] = nil
+            elseif v~=nil and os.clock()-v > 5 then
+                -- If you asked for an update more than 5 seconds ago and haven't gotten one yet, try again
+                indices_under_investigation[i] = nil
+            end
+        end
+        
+        -- After clearing out the ones that finally have proper information and those that you're waiting on, you're left with the novel ones that you haven't attempted to contact anyone about yet
         for i,v in pairs(novel_indices) do
             local character = windower.ffxi.get_mob_by_index(i)
             if v~= nil and (not character or character.id == 0) then
                 -- We've waited a packet cycle since the first occurance of the index and real information hasn't shown up yet, so request another packet
                 print('Sent out an information request for index',i)
                 windower.packets.inject_outgoing(0x016,string.char(0x016,4,0,0,i%256,math.floor(i/256),0,0))
+                indices_under_investigation[i] = os.clock()
             elseif v ~= nil then
+                -- Novel index, successfully updated through normal methods.
                 valid_indices[i] = distance({X=character.x,Y=character.y,Z=character.z},player)
+                indices_under_investigation[i] = nil
             end
             novel_indices[i] = nil -- Reset it in anticipation of a new packet
         end
+        
         last_novel = nil
     end
     if not inj and id == 0x00D or id == 0x00E then
