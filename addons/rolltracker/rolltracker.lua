e9b837e39fb539de78067190bd09db1df146b91f
MilkMansKid <wastedfx@gmail.com>
2017-04-30 08:11:19 -0400

    Geo Rolls #s

--- addons/rolltracker/rolltracker.lua
+++ addons/rolltracker/rolltracker.lua
@@ -25,7 +25,7 @@
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'RollTracker'
-_addon.version = '1.4.0.0'
+_addon.version = '1.5.0.0'
 _addon.author = 'Balloon'
 _addon.command = 'rolltracker'
 
@@ -128,7 +128,7 @@ windower.register_event('load', function()
         ['Blitzer\'s'] = {2,3.4,4.5,11.3,5.3,6.4,7.2,8.3,1.5,10.2,12.1,'-?', '% Attack delay reduction',4, 1, {4,11080, 5}},
         ['Courser\'s'] = {'?','?','?','?','?','?','?','?','?','?','?','?',' Snapshot',3, 0},
         ['Runeist\'s'] = {'?','?','?','?','?','?','?','?','?','?','?','?',' Magic Evasion',4, 0},
-        ['Naturalist\'s'] = {'?','?','?','?','?','?','?','?','?','?','?','?',' Enhancing Magic Duration',3, 0}
+        ['Naturalist\'s'] = {6,7,15,8,9,10,5,11,12,14,20,'-5','% Enhancing Magic Duration',3, 1}
     }
 
     rollInfo = {}
