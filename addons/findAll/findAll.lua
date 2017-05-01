0ff378de073d7fffae61aa92d6061293b0d810ce
Kinkles <toiddealer@gmail.com>
2017-05-01 05:46:55 -0400

    findall - key item display setting
    Added a setting to stop key items from being displayed that was added last commit.

--- addons/findAll/findAll.lua
+++ addons/findAll/findAll.lua
@@ -28,7 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name    = 'findAll'
 _addon.author  = 'Zohno'
-_addon.version = '1.201704052'
+_addon.version = '1.20170501'
 _addon.commands = {'findall'}
 
 require('chat')
@@ -48,6 +48,7 @@ res = require('resources')
 defaults = {}
 defaults.Track = ''
 defaults.Tracker = {}
+defaults.Tracker.Key_Item_Display = true
 
 settings = config.load(defaults)
 
@@ -282,7 +283,7 @@ function search(query, export)
     end
     
     for id, names in pairs(key_item_names) do
-        if terms_pattern == '' or key_item_names[id].name:find(terms_pattern) then
+        if terms_pattern == '' or key_item_names[id].name:find(terms_pattern) and settings.Tracker.Key_Item_Display == true then
             results_key_items:add(id)
         end
     end
