173a8d90f53ae52d289dc7e8c0d1e374998088ac
Byrth <squarefocus@hotmail.com>
2018-05-22 22:14:54 -0400

    GearSwap v0.932
    Should fix a problem identifying worn gear by bag.

--- addons/GearSwap/gearswap.lua
+++ addons/GearSwap/gearswap.lua
@@ -25,7 +25,7 @@
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 _addon.name = 'GearSwap'
-_addon.version = '0.931'
+_addon.version = '0.932'
 _addon.author = 'Byrth'
 _addon.commands = {'gs','gearswap'}
 
