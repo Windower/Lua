f1ea7c1cc6399cd19fd3602d075befafbfde3627
from20020516 <from20020516@gmail.com>
2018-05-30 04:08:30 +0900

    update checkparam.lua

--- addons/checkparam/README.md
+++ addons/checkparam/README.md
@@ -1,11 +1,18 @@
 # checkparam
-  - //cp
-	-  装備中アイテムの任意のプロパティを合計して表示します。
-	- displays the total of optional properties of the item being equipped.
-  - //cp debug
-	- 装備中アイテムの全てのプロパティを装備ごとに表示します。
-	- displays all the properties of the item being equipped for each equipment.
-## settings.xml
-  - 表示させるプロパティをジョブごとに定義します。
-  - define the properties to be displayed for each job.
-	- || 改行/new line starts
+
+- `/check` OR `/c` (in-game command)
+  - 「調べる」したとき、対象が装備しているアイテムのうち任意のプロパティを合計して表示します。
+  - whenever you /check any player, displays the total of property of current equipments.(defined in `setting.xml`)
+
+## data/settings.xml (auto-generated)
+- 表示させるプロパティをジョブごとに定義します。
+  - `|` 区切り記号 divide each property
+  - `NON`は/anon時に使用
+  - 召喚獣: 飛竜: オートマトン: 羅盤: などペットに関するプロパティは全て`pet: `で指定します。
+
+
+- Checkparam uses `setting.xml` to define the properties you want to be displayed.
+  - You can define the properties for each job.
+  - `NON` means target status is /anon
+  -  `pet: ` define properties for all pets, which means avatar,wyvern,automaton,luopan.
+- If there’s something wrong,or something strange,please tell me on Twitter [@from20020516](https://twitter.com/from20020516) **with simple English**. Thank you!
--- addons/checkparam/checkparam.lua
+++ addons/checkparam/checkparam.lua
@@ -1,150 +1,3 @@
-_addon.name = 'checkparam'
-_addon.author = 'from20020516'
-_addon.version = '1.0'
-_addon.commands = {'cp','checkparam'}
-res,extdata,config = require('resources'),require('extdata'),require('config')
-settings = config.load('data/settings.xml')
-function key_define()
-    job_define = settings[windower.ffxi.get_player().main_job:lower()]
-    return job_define and windower.regex.split(job_define,'[|]')
-        or {'store tp','quadruple attack','triple attack','double attack','',
-            'magic accuracy','fast cast','haste','dual wield','',
-            'magic attack bonus','magic burst damage','magic damage','',
-            'enmity','cure potency'}
-end
-function split_text(txt,id,arg)
-    for key,value in string.gmatch(txt,"/?([%D]-):?([%+%-]?[0-9]+)%%?%s?") do
-        local key = windower.regex.replace(string.lower(key),'(\\"|\\.|\\s$)','')
-        local key = integrate[key] or key
-        local key = arg and arg..key or key
-        tbl[key] = tonumber(value) + (tbl[key] or 0)
-        if debug then
-            local color = windower.regex.match(key,'enhanced') and 170
-            windower.add_to_chat(color or 72,'['..id..']'..res.items[id].en..'['..key..'] '..value..' '..tbl[key])
-        end
-    end
-end
-function checkparam(arg1)
-    debug = arg1=='debug'
-    tbl,txt={},
-    windower.send_command('input /checkparam <me>')
-    for slot,bag in pairs(windower.ffxi.get_items().equipment) do
-        gtbl = windower.ffxi.get_items().equipment
-        if not string.find(slot,'_bag') then
-            gear = windower.ffxi.get_items(gtbl[slot..'_bag'],gtbl[slot])
-            if gtbl[slot] > 0 and gear.id < 65535 then
-                if res.item_descriptions[gear.id] then
-                    local txt = windower.regex.split(res.item_descriptions[gear.id].en,"(Pet|Avatar|Automaton|Wyvern|Luopan): ")
-                    for i,v in pairs(windower.regex.split(txt[1],"\n")) do
-                        split_text(v,gear.id)
-                    end
-                    if txt[2] then
-                        local txt = windower.regex.replace(txt[2],'\n',' ')
-                        split_text(txt,gear.id,'pet: ')
-                    end
-                end
-                if gear.extdata and extdata.decode(gear).type == 'Augmented Equipment' then
-                    for slot,augment in pairs(extdata.decode(gear).augments) do
-                        split_text(augment,gear.id)
-                    end
-                end
-                if enhanced[res.items[gear.id].en] then
-                    local txt = string.split(string.gsub(enhanced[res.items[gear.id].en], "([+-:][0-9]+)",",%1"),",")
-                    tbl[txt[1]] = tonumber(txt[2]) + (tbl[txt[1]] or 0)
-                    if debug then
-                        windower.add_to_chat(170,'['..gear.id..']'..res.items[gear.id].en..'['..txt[1]..'] '..txt[2]..' '..tbl[txt[1]])
-                    end
-                end
-            end
-        end
-    end
-    windower.add_to_chat(69, '//checkparam')
-    for index,key in pairs(key_define()) do
-        txt = key == '' and txt..'\n' or (txt or '')..'['..key..'] '..(tbl[string.lower(key)] or 0)..' '
-    end
-    windower.add_to_chat(70, txt)
-    collectgarbage()
-end
-integrate={
---[[integrate same property.information needed for development. @from20020516]]
-['quad atk']='quadruple attack',
-['triple atk']='triple attack',
-['double atk']='double attack',
-['dblatk']='double attack',
-['blood pact ability delay']='blood pact delay',
-['blood pact ability delay ii']='blood pact delay ii',
-['blood pact ab. del. ii']='blood pact delay ii',
-['blood pact recast time ii']='blood pact delay ii',
-['blood pact dmg']='blood pact damage',
-['enhancing magic duration']='enhancing magic effect duration',
-['eva']='evasion',
-['indicolure spell duration']='indicolure effect duration',
-['mag eva']='magic evasion',
-['magic atk bonus']='magic attack bonus',
-['magatkbns']='magic attack bonus',
-['mag atk bonus']='magic attack bonus',
-['mag acc']='magic accuracy',
-['magic burst dmg']='magic burst damage',
-['mag dmg']='magic damage',
-['crithit rate']='critical hit rate',
-}
-enhanced={
-['Anhur Robe']='fast cast+10',
-['Atinian Staff +1']='fast cast+2',
-['Brutal Earring']='double attack+5',
-['Charis Necklace']='dual wield+3',
-['Cirque Necklace']='martial arts+10',
-['Cizin Helm']='fast cast+5',
-['Conveyance Cape']='elemental siphon+30',
-['Debilis Medallion']='cursna+15',
-['Earthcry Earring']='stoneskin+10',
-['Eirene\'s Manteel']='fast cast+10',
-['Emphatikos Rope']='aquaveil+1',
-['Ephedra Ring']='cursna+10',
-['Estq. Earring']='fast cast+2',
-['Excelsis Ring']='drain and aspir potency+5',
-['Ferine Earring']='rewards+2',
-['Gende. Galoshes']='cursna+10',
-['Gendewitha Gages']='fast cast+7',
-['Geomancy Pants']='fast cast+10',
-['Haoma\'s Ring']='cursna+15',
-['Hearty Earring']='increases resistance to all status ailments+5',
-['Iga Mimikazari']='dual wield+1',
-['Impulse Belt']='snapshot+3',
-['Kenkonken']='martial arts+55',
-['Lifestream Cape']='fast cast+7',
-['Locus Ring']='magic burst damage+5',
-['Loquac. Earring']='fast cast+2',
-['Majorelle Shield']='fast cast+5',
-['Malison Medallion']='cursna+10',
-['Manibozho Gloves']='snapshot+4',
-['Mending Cape']='cursna+15',
-['Nahtirah Hat']='fast cast+10',
-['Nahtirah Trousers']='snapshot+9',
-['Navarch\'s Mantle']='snapshot+5',
-['Nusku\'s Sash']='dual wield+5',
-['Oneiros Rope']='occult acumen+20',
-['Orison Locket']='fast cast+5',
-['Orunmila\'s Torque']='fast cast+5',
-['Orvail Pants +1']='fast cast+5',
-['Patentia Sash']='dual wield+5',
-['Prolix Ring']='fast cast+2',
-['Raider\'s Bmrng.']='dual wield+3',
-['Static Earring']='magic burst damage+5',
-['Savant\'s Earring']='sublimation+1',
-['Shaolin Belt']='martial arts+10',
-['Siegel Sash']='stoneskin+20',
-['Spharai']='counter+14',
-['Stone Gorget']='stoneskin+30',
-['Suppanomimi']='dual wield+5',
-['Swith Cape +1']='fast cast+4',
-['Swith Cape']='fast cast+3',
-['Veela Cape']='fast cast+1',
-['Witful Belt']='fast cast+3',
-['Yagrush']='divine benison+3',
-}
-windower.register_event('addon command', checkparam)
-windower.send_command('cp')
 --[[
 Copyright © 2018, from20020516
 All rights reserved.
@@ -170,5 +23,212 @@ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]]
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.]]
+
+_addon.name = 'Checkparam'
+_addon.author = 'from20020516'
+_addon.version = '1.1'
+_addon.commands = {'cp','checkparam'}
+
+require('logger')
+res = require('resources')
+extdata = require('extdata')
+config = require('config')
+packets = require('packets')
+
+defaults = {
+  NON = 'fast cast',
+  WAR = 'store tp|double attack|triple attack|quadruple attack|weapon skill damage',
+  MNK = 'store tp|double attack|triple attack|quadruple attack|martial arts|subtle blow',
+  WHM = 'cure potency|cure potency ii|fast cast|cure spellcasting time|enmity|healing magic casting time|divine benison|damage taken|physical damage taken|magic damage taken',
+  BLM = 'magic attack bonus|magic burst damage|magic burst damage ii|int|magic accuracy|magic damage|fast cast|elemental magic casting time',
+  RDM = 'magic attack bonus|magic burst damage|magic burst damage ii|magic accuracy|fast cast|enfeebling magic skill|enhancing magic skill|store tp|dual wield',
+  THF = 'store tp|double attack|triple attack|quadruple attack|dual wield',
+  PLD = 'enmity|damage taken|physical damage taken|magic damage taken|spell inturruption rate|pharanx|cure potency|fastcast',
+  DRK = 'store tp|double attack|triple attack|quadruple attack|weapon skill damage',
+  BST = 'pet: double attack|pet: magic attack bonus|pet: damage taken',
+  BRD = 'all songs|song effect duration|fast cast|song spellcasting time',
+  RNG = 'store tp|snapshot|rapid shot|weapon skill damage',
+  SAM = 'store tp|double attack|triple attack|quadruple attack|weapon skill damage',
+  NIN = 'store tp|double attack|triple attack|quadruple attack|subtle blow',
+  DRG = 'store tp|double attack|triple attack|quadruple attack|weapon skill damage',
+  SMN = 'blood pact delay|blood pact delay ii|blood pact damage|avatar perpetuation cost|pet: magic attack bonus|pet: str|pet: double attack',
+  BLU = 'store tp|double attack|triple attack|quadruple attack|critical hit rate|critical hit damage|weapon skill damage|fast cast|magic attack bonus|magic accuracy|cure potency',
+  COR = 'store tp|snapshot|rapid shot|fast cast|cure potency|magic accuracy|magic attack bonus|magic damage|weapon skill damage',
+  PUP = 'pet: hp|pet: damage taken|pet: regen|martial arts|store tp|double attack|triple attack|quadruple attack',
+  DNC = 'store tp|double attack|triple attack|quadruple attack',
+  SCH = 'magic attack bonus|magic burst damage|magic burst damage ii|magic accuracy|magic damage|fast cast|elemental magic casting time|cure potency|enh mag eff dur|enhancing magic effect duration',
+  GEO = 'pet: regen|pet: damage taken|indicolure effect duration|indi eff dur|fast cast|magic evasion',
+  RUN = 'enmity|damage taken|physical damage taken|magic damage taken|spell inturruption rate|pharanx|inquartata|fastcast',
+  debugmode = false,
+}
+settings = config.load(defaults)
+
+tbl = {}
+
+windower.register_event('incoming chunk',function(id,data)
+  if id == 0x0C9 then
+    local p = packets.parse('incoming',data)
+    if p['Type'] == 3 then
+      local count = p['Count']
+      if count == 1 then
+        get_text(p['Item'],p['ExtData'])
+      else
+        for i=1,count do
+          get_text(p['Item '..i],p['ExtData '..i])
+        end
+      end
+    elseif p['Type'] == 1 then --metadata
+      local t = windower.ffxi.get_mob_by_id(p['Target ID'])
+      local mjob = res.jobs[p['Main Job']].ens
+      local sjob = res.jobs[p['Sub Job']].ens
+      show_results(t.name,mjob,sjob)
+    end
+  end
+end)
+
+function get_text(id,data)
+  config.reload(settings)
+  local descriptions = res.item_descriptions[id]
+  local helptext = descriptions and descriptions.english or '' --for 'vanilla' items. e.g. Moonshade Earring
+  local stats = windower.regex.split(helptext,'(Pet|Avatar|Automaton|Wyvern|Luopan): ')
+  for i,v in ipairs(windower.regex.split(stats[1],'\n')) do
+    split_text(id,v)
+  end
+  if stats[2] then
+    local pet_text = windower.regex.replace(stats[2],'\n',' ')
+    split_text(id,pet_text,'pet: ')
+  end
+  local ext = extdata.decode({id=id,extdata=data})
+  if ext.augments then
+    for i,v in ipairs(ext.augments) do
+      split_text(id,v)
+    end
+  end
+  if enhanced[id] then
+    local stats = enhanced[id]:gsub('([+-:][0-9]+)',',%1'):split(',')
+    tbl[stats[1]] = tonumber(stats[2]) + (tbl[stats[1]] or 0)
+    if settings.debugmode then
+      log(id,res.items[id].english,stats[1],stats[2],tbl[stats[1]])
+    end
+  end
+end
+
+function split_text(id,text,arg)
+  for key,value in string.gmatch(text,'/?([%D]-):?([%+%-]?[0-9]+)%%?%s?') do
+    local key = windower.regex.replace(string.lower(key),'(\\"|\\.|\\s$)','')
+    local key = integrate[key] or key
+    local key = arg and arg..key or key
+    tbl[key] = tonumber(value) + (tbl[key] or 0)
+    if settings.debugmode then
+      log(id,res.items[id].english,key,value,tbl[key])
+    end
+  end
+end
+
+function show_results(name,mjob,sjob)
+  local stats = settings[mjob]
+  local head = '<'..mjob..'/'..sjob..'>'
+  windower.add_to_chat(160,string.color(name,1,160)..': '..string.color(head,160,160))
+  if mjob == 'NON' then
+    notice('Unknown job because /anon state. instead display <NON>.')
+  end
+  for index,key in ipairs(windower.regex.split(stats,'[|]')) do
+    local value = tbl[string.lower(key)]
+    local color = {value and 1 or 160,value and 166 or 160}
+    windower.add_to_chat(160,' ['..string.color(key,color[1],160)..'] '..string.color(tostring(value),color[2],160))
+  end
+  tbl = {}
+  collectgarbage()
+end
+
+windower.register_event('load',function()
+  print('Checkparam: Activate with in-game /check command.')
+end)
+
+integrate = {
+  --[[integrate same property.information needed for development. @from20020516]]
+  ['quad atk'] = 'quadruple attack',
+  ['triple atk'] = 'triple attack',
+  ['double atk'] = 'double attack',
+  ['dblatk'] = 'double attack',
+  ['blood pact ability delay'] = 'blood pact delay',
+  ['blood pact ability delay ii'] = 'blood pact delay ii',
+  ['blood pact ab. del. ii'] = 'blood pact delay ii',
+  ['blood pact recast time ii'] = 'blood pact delay ii',
+  ['blood pact dmg'] = 'blood pact damage',
+  ['enhancing magic duration'] = 'enhancing magic effect duration',
+  ['eva'] = 'evasion',
+  ['indicolure spell duration'] = 'indicolure effect duration',
+  ['mag eva'] = 'magic evasion',
+  ['magic atk bonus'] = 'magic attack bonus',
+  ['magatkbns'] = 'magic attack bonus',
+  ['mag atk bonus'] = 'magic attack bonus',
+  ['mag acc'] = 'magic accuracy',
+  ['magic burst dmg'] = 'magic burst damage',
+  ['mag dmg'] = 'magic damage',
+  ['crithit rate'] = 'critical hit rate',
+}
+enhanced = {
+  [10392] = 'cursna+10', --Malison Medallion
+  [10393] = 'cursna+15', --Debilis Medallion
+  [10394] = 'fast cast+5', --Orunmila's Torque
+  [10469] = 'fast cast+10', --Eirene's Manteel
+  [10752] = 'fast cast+2', --Prolix Ring
+  [10790] = 'cursna+10', --Ephedra Ring
+  [10791] = 'cursna+15', --Haoma's Ring
+  [10802] = 'fast cast+5', --Majorelle Shield
+  [10826] = 'fast cast+3', --Witful Belt
+  [10838] = 'dual wield+5', --Patentia Sash
+  [11000] = 'fast cast+3', --Swith Cape
+  [11001] = 'fast cast+4', --Swith Cape +1
+  [11037] = 'stoneskin+10', --Earthcry Earring
+  [11051] = 'increases resistance to all status ailments+5', ----Hearty Earring
+  [11544] = 'fast cast+1', --Veela Cape
+  [11602] = 'martial arts+10', --Cirque Necklace
+  [11603] = 'dual wield+3', --Charis Necklace
+  [11615] = 'fast cast+5', --Orison Locket
+  [11707] = 'fast cast+2', --Estq. Earring
+  [11711] = 'rewards+2', --Ferine Earring
+  [11715] = 'dual wield+1', --Iga Mimikazari
+  [11722] = 'sublimation+1', --Savant's Earring
+  [11732] = 'dual wield+5', --Nusku's Sash
+  [11734] = 'martial arts+10', --Shaolin Belt
+  [11735] = 'snapshot+3', --Impulse Belt
+  [11753] = 'aquaveil+1', --Emphatikos Rope
+  [11775] = 'occult acumen+20', --Oneiros Rope
+  [11856] = 'fast cast+10', --Anhur Robe
+  [13177] = 'stoneskin+30', --Stone Gorget
+  [14739] = 'dual wield+5', --Suppanomimi
+  [14812] = 'fast cast+2', --Loquac. Earring
+  [14813] = 'double attack+5', --Brutal Earring
+  [15857] = 'drain and aspir potency+5', --Excelsis Ring
+  [15960] = 'stoneskin+20', --Siegel Sash
+  [15962] = 'magic burst damage+5', --Static Earring
+  [16209] = 'snapshot+5', --Navarch's Mantle
+  [19062] = 'divine benison+1', --Yagrush80
+  [19082] = 'divine benison+2', --Yagrush85
+  [19260] = 'dual wield+3', --Raider's Bmrng.
+  [19614] = 'divine benison+3', --Yagrush90
+  [19712] = 'divine benison+3', --Yagrush95
+  [19821] = 'divine benison+3', --Yagrush99
+  [19950] = 'divine benison+3', --Yagrush99+
+  [20509] = 'counter+14', --Spharai119AG
+  [20511] = 'martial arts+55', --Kenkonken119AG
+  [21062] = 'divine benison+3', --Yagrush119
+  [21063] = 'divine benison+3', --Yagrush119+
+  [21078] = 'divine benison+3', --Yagrush119AG
+  [21201] = 'fast cast+2', --Atinian Staff +1
+  [27768] = 'fast cast+5', --Cizin Helm
+  [27775] = 'fast cast+10', --Nahtirah Hat
+  [28054] = 'fast cast+7', --Gendewitha Gages
+  [28058] = 'snapshot+4', --Manibozho Gloves
+  [28184] = 'fast cast+5', --Orvail Pants +1
+  [28197] = 'snapshot+9', --Nahtirah Trousers
+  [28206] = 'fast cast+10', --Geomancy Pants
+  [28335] = 'cursna+10', --Gende. Galoshes
+  [28582] = 'magic burst damage+5', --Locus Ring
+  [28619] = 'cursna+15', --Mending Cape
+  [28631] = 'elemental siphon+30', --Conveyance Cape
+  [28637] = 'fast cast+7', --Lifestream Cape
+}
