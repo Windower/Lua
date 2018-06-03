eee17318d4cafb23e67452b65eae3aba0053ca35
from20020516 <from20020516@gmail.com>
2018-06-03 23:34:31 +0900

    global var `ext` changed to local.

--- addons/MyHome/MyHome.lua
+++ addons/MyHome/MyHome.lua
@@ -55,7 +55,7 @@ function search_item()
         local item = item_array[stats.id]
         local set_equip = windower.ffxi.set_equip
         if item then
-            ext = extdata.decode(item)
+            local ext = extdata.decode(item)
             local enchant = ext.type == 'Enchanted Equipment'
             local recast = enchant and ext.charges_remaining > 0 and math.max(ext.next_use_time+18000-os.time(),0)
             local usable = recast and recast == 0
--- addons/MyHome/README.md
+++ addons/MyHome/README.md
@@ -4,7 +4,7 @@
 ## Command
 - `//mh` OR `//warp`
 - Priorities are:
-    1. Warp II <me\>
-    2. Warp Ring
-    3. Warp Cudgel
-    4. Instant Warp
+    1. Warp II <me\> - require main or sub job BLM.
+    2. Warp Ring - search inventory and wardrobes.
+    3. Warp Cudgel - search inventory and wardrobes.
+    4. Instant Warp - search inventory.
