2f822f31cbacca8aeb64e96e77c97bc57c8ada5e
KenshiDRK <jaciarriaga@ono.com>
2019-06-10 09:58:36 +0200

    Added Larceny message
    Added Larceny message

--- addons/battlemod/parse_action_packet.lua
+++ addons/battlemod/parse_action_packet.lua
@@ -341,15 +341,15 @@ function simplify_message(msg_ID)
     local msg = res.action_messages[msg_ID][language]
     local fields = fieldsearch(msg)
 
-    if simplify and not T{23,64,129,133,139,140,204,210,211,212,213,214,244,350,442,453,516,531,557,565,582,674}:contains(msg_ID) then
-        if T{93,273,522,653,654,655,656,85,284,75,114,156,189,248,283,312,323,336,351,355,408,422,423,425,659,158,245,324,658}:contains(msg_ID) then
+    if simplify and not T{23,64,129,133,139,140,204,210,211,212,213,214,244,350,442,516,531,557,565,582,674}:contains(msg_ID) then
+        if T{93,273,522,653,654,655,656,85,284,75,114,156,189,248,283,312,323,336,351,355,408,422,423,425,453,659,158,245,324,658}:contains(msg_ID) then
             fields.status = true
         end
         if msg_ID == 31 then
             fields.actor = true
         end    
         if (msg_ID > 287 and msg_ID < 303) or (msg_ID > 384 and msg_ID < 399) or (msg_ID > 766 and msg_ID < 771) or
-            T{152,161,162,163,165,229,384,603,652}:contains(msg_ID) then
+            T{152,161,162,163,165,229,384,453,603,652}:contains(msg_ID) then
                 fields.ability = true
         end
         
