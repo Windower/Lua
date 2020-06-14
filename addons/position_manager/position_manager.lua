8645df2d0ee31cfee4c7d457e7f00d60485d4c2c
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2020-06-14 16:28:23 +0100

    Update position_manager.lua

--- addons/position_manager/position_manager.lua
+++ addons/position_manager/position_manager.lua
@@ -1,4 +1,4 @@
---Copyright (c) 2020, Lili
+--Copyright © 2020, Lili
 --All rights reserved.
 
 --Redistribution and use in source and binary forms, with or without
@@ -33,9 +33,11 @@ if not windower.file_exists(windower.windower_path .. '\\plugins\\WinControl.dll
     print('position_manager: error - Please install the WinControl plugin in the launcher.')
     windower.send_command('lua u position_manager')
     return
+else
+    windower.send_command('load wincontrol')
 end
 
-config = require 'config'
+config = require('config')
 
 default = {
     x = 0,
@@ -45,24 +47,26 @@ default = {
 settings = config.load(default)
 
 function move(settings)
-    windower.send_command('load wincontrol')
-    coroutine.sleep(0.5)
     windower.send_command('wincontrol move %s %s':format(settings.x,settings.y))
-    coroutine.sleep(0.5)
-    windower.send_command('unload wincontrol')
 end
 
-function handle_commands(cmd,name,pos_x,pos_y)
-    local cmd = cmd or 'help'
-    
+function handle_commands(cmd,pos_x,pos_y,name)
+    cmd = cmd:lower() or 'help'
+
     if cmd == 'r' then
         windower.send_command('lua r position_manager')
     elseif cmd == 'set' then
+        if not name or name == ':current' then
+            name = windower.ffxi.get_player().name
+        elseif name == ':all' then
+            name = 'all'
+        end
+
         if name and pos_x and pos_y then
             settings.x = tonumber(pos_x)
             settings.y = tonumber(pos_y)
             config.save(settings,name)
-            
+
             if windower.ffxi.get_info().logged_in then
                 player_name = windower.ffxi.get_player().name
                 if name:lower() == player_name:lower() then
@@ -72,11 +76,12 @@ function handle_commands(cmd,name,pos_x,pos_y)
             end
         end
     elseif cmd == 'help' then
-        windower.add_to_chat(207,'position_manager: Usage: //pm set <Character|all> <x> <y> ')
-    else 
+        windower.add_to_chat(207,'position_manager: Usage: //pm set <x> <y> [Charactername|:all|:current]')
+        windower.add_to_chat(207,'position_manager: See the readme for details.')
+    else
         windower.add_to_chat(207,'position_manager: %s command not found.':format(cmd))
         windower.send_command('pm help')
-    end    
+    end
 end
 
 config.register(settings,move)
