f5350f3175843351c0e18ff3fc174ce6d5c190b4
Chiaia <chiaiaxi@gmail.com>
2020-09-18 19:02:31 -0400

    Update packets.lua

--- addons/libs/packets.lua
+++ addons/libs/packets.lua
@@ -164,7 +164,6 @@ parse = function(fields, data, index, max, lookup, depth)
                         bits = sizes[type] * type_count
 
                         field.ctype = '%s[%u]':format(type, type_count)
-                        field.vlstring = type == 'char'
 
                         count = max
                     end
@@ -263,10 +262,6 @@ local make_pack_string = function(field)
 
     if pack_ids[ctype] then
         return pack_ids[ctype]
-    end
-
-    if field.vlstring then
-        return 'z'
     end
 
     local type_name, number = ctype:match(array_pattern)
@@ -459,4 +454,3 @@ Redistribution and use in source and binary forms, with or without modification,
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Windower BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
-
