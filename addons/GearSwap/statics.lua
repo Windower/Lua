86d935ccde3b333d36594bd8c2b0ab74cd153b9a
Nifim <TesaRollens@gmail.com>
2020-11-09 08:56:41 -0800

    Update bstpet_range
    new pet moves added to resources, need to change max value to reflect new value.

--- addons/GearSwap/statics.lua
+++ addons/GearSwap/statics.lua
@@ -54,7 +54,7 @@ for i,v in pairs(res.bags) do
     bag_string_lookup[to_windower_bag_api(v.en)]=i
 end
     
-bstpet_range = {min=672,max=782} -- Range of the JA resource devoted to BST jugpet abilities
+bstpet_range = {min=672,max=798} -- Range of the JA resource devoted to BST jugpet abilities
     
 delay_map_to_action_type = {['Ability']=3,['Magic']=20,['Ranged Attack']=10,['Item']=10,['Monster Move']=10,['Interruption']=3}
     
