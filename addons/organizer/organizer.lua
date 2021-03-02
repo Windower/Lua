191bc6fa1db5ef53dacb37a9ea0172b11bea99b0
Chiaia <chiaiaxi@gmail.com>
2021-03-02 08:51:48 -0500

    Fixes for Kenshi Slip data so it doesn't break stuff.
    SE skipped an index so we're using 0 as it is not part of item ids as a placeholder.

--- addons/organizer/organizer.lua
+++ addons/organizer/organizer.lua
@@ -186,8 +186,10 @@ function options_load( )
             slip_lists = require('slips')
             for slip_id,slip_list in pairs(slip_lists.items) do
                 for item_id in slip_list:it() do
-                    _retain[item_id] = "moogle slip"
-                    org_debug("settings", "Adding ("..res.items[item_id].english..') to slip retain list')
+                    if item_id ~= 0 then
+                        _retain[item_id] = "moogle slip"
+                        org_debug("settings", "Adding ("..res.items[item_id].english..') to slip retain list')
+                    end
                 end
             end
         end
