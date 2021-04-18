21ad3ea2f98e2a33e5ea2dc2f425b5b56a18ecbc
Ian Anderson <iananderson@berkeley.edu>
2021-04-18 10:11:10 -0700

    Update Stubborn.lua

--- addons/Stubborn/Stubborn.lua
+++ addons/Stubborn/Stubborn.lua
@@ -9,49 +9,46 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of <addon name> nor the
+    * Neither the name of Stubborn nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL <your name> BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL Arico BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. --]]
-
+ b275nMv
 _addon.name = 'Stubborn'
 _addon.author = 'Arico'
 _addon.version = '1'
 _addon.command = 'stubborn'
 
-require('pack')
-require('strings')
 require('logger')
 packets = require('packets')
 
-
 windower.register_event('outgoing chunk', function(id, original, modified, injected, blocked)
-    if id == 0x01a then
+    if id == 0x01A and not injected then
         local p = packets.parse('outgoing', original)
-        if p['Category'] == 5 and not injected then
-            log('You are too stubborn to call for help! Use //stubborn to call for help.')
+        if p['Category'] == 5 then
             return true
         end
     end
 end)
 
 windower.register_event('addon command', function(...)
-    local target = windower.ffxi.get_mob_by_target("t")
+    local target = windower.ffxi.get_mob_by_target('t')
     if target and target.claim_id ~= 0 then  
-        local p = packets.new('outgoing', 0x1a, {
-                ['Target'] = target['id'],
-                ['Target Index'] = target['index'],
-                ['Category'] = 5,})
+        local p = packets.new('outgoing', 0x01A, {
+            ['Target'] = target['id'],
+            ['Target Index'] = target['index'],
+            ['Category'] = 5,
+            })
         packets.inject(p)
     end
 end)
