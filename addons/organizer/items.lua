f63d7082015e93f2a19b5ff9e61bf082fbfe2d65
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2021-06-10 22:29:48 +0100

    Checking for valid_target

--- addons/organizer/items.lua
+++ addons/organizer/items.lua
@@ -53,9 +53,9 @@ do
         for _,moogle in pairs(moogles) do
             if not moogle.x then
                 local t = windower.ffxi.get_mob_by_index(moogle.index)
-                moogle.x, moogle.y = t.x, t.y
+                moogle.x, moogle.y, moogle.valid_target = t.x, t.y, t.valid_target
             end
-            if moogle.x then
+            if moogle.x and moogle.valid_target then
                 local dx = player.x - moogle.x
                 local dy = player.y - moogle.y
                 if dx*dx+dy*dy < 36 then
