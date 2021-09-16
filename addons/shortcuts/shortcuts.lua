82e6653d2a302bbf5492ee553570b0c89bda8ca4
Byrth <squarefocus@hotmail.com>
2021-09-15 22:07:44 -0400

    version bumps

--- addons/shortcuts/shortcuts.lua
+++ addons/shortcuts/shortcuts.lua
@@ -24,7 +24,7 @@
 --(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 --SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-_addon.version = '2.902'
+_addon.version = '2.903'
 _addon.name = 'Shortcuts'
 _addon.author = 'Byrth'
 _addon.commands = {'shortcuts'}
