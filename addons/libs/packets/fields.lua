5ade870f842c7be518f6d9112b2592c25072ee2c
RubenatorX <RubenatorX@Gmail.com>
2021-11-11 07:51:27 -0700

    Update Master Level 2
    Changed to use boolbits and added Master Levels menu display data

--- addons/libs/packets/fields.lua
+++ addons/libs/packets/fields.lua
@@ -1519,6 +1519,45 @@ fields.incoming[0x01B] = L{
     {ctype='unsigned char',     label='Mentor Icon',      fn=e+{'mentor icon'}},-- 65
     {ctype='unsigned char',     label='Mastery Rank'},                          -- 66
     {ctype='unsigned char',     label='_unknown8'},                             -- 67
+    {ctype='bit[1]',            label='_junk1'},                                -- 68
+    {ctype='boolbit',           label='Warrior Master'},                        -- 68   Indicates if the job is mastered, but only after receiving "Master Breaker" KI. Used to populate "Master Levels" Menu
+    {ctype='boolbit',           label='Monk Master'},                           -- 68
+    {ctype='boolbit',           label='White Mage Master'},                     -- 68
+    {ctype='boolbit',           label='Black Mage Master'},                     -- 68
+    {ctype='boolbit',           label='Red Mage Master'},                       -- 68
+    {ctype='boolbit',           label='Thief Master'},                          -- 68
+    {ctype='boolbit',           label='Paladin Master'},                        -- 68
+    {ctype='boolbit',           label='Dark Knight Master'},                    -- 69
+    {ctype='boolbit',           label='Beastmaster Master'},                    -- 69
+    {ctype='boolbit',           label='Bard Master'},                           -- 69
+    {ctype='boolbit',           label='Ranger Master'},                         -- 69
+    {ctype='boolbit',           label='Samurai Master'},                        -- 69
+    {ctype='boolbit',           label='Ninja Master'},                          -- 69
+    {ctype='boolbit',           label='Dragoon Master'},                        -- 69
+    {ctype='boolbit',           label='Summoner Master'},                       -- 69
+    {ctype='boolbit',           label='Blue Mage Master'},                      -- 6A
+    {ctype='boolbit',           label='Corsair Master'},                        -- 6A
+    {ctype='boolbit',           label='Puppetmaster Master'},                   -- 6A
+    {ctype='boolbit',           label='Dancer Master'},                         -- 6A
+    {ctype='boolbit',           label='Scholar Master'},                        -- 6A
+    {ctype='boolbit',           label='Geomancer Master'},                      -- 6A
+    {ctype='boolbit',           label='Rune Fencer Master'},                    -- 6A
+    {ctype='bit[1]',            label='_junk2'},                                -- 6A
+    {ctype='unsigned short',    label='_junk3'},                                -- 6B
+    {ctype='unsigned char',     label='Warrior Master Level'},                  -- 6D
+    {ctype='unsigned char',     label='Monk Master Level'},                     -- 6E
+    {ctype='unsigned char',     label='White Mage Master Level'},               -- 6F
+    {ctype='unsigned char',     label='Black Mage Master Level'},               -- 70
+    {ctype='unsigned char',     label='Red Mage Master Level'},                 -- 71
+    {ctype='unsigned char',     label='Thief Master Level'},                    -- 72
+    {ctype='unsigned char',     label='Paladin Master Level'},                  -- 73
+    {ctype='unsigned char',     label='Dark Knight Master Level'},              -- 74
+    {ctype='unsigned char',     label='Beastmaster Master Level'},              -- 75
+    {ctype='unsigned char',     label='Bard Master Level'},                     -- 76
+    {ctype='unsigned char',     label='Dancer Master Level'},                   -- 77
+    {ctype='unsigned char',     label='Scholar Master Level'},                  -- 78
+    {ctype='unsigned char',     label='Geomancer Master Level'},                -- 79
+    {ctype='unsigned char',     label='Rune Fencer Master Level'},              -- 7A
 }
 
 -- Inventory Count
@@ -2993,7 +3032,7 @@ fields.incoming[0x061] = L{
     {ctype='unsigned int',      label='_junk2'},                                -- 60
     {ctype='unsigned char',     label='_unknown7'},                             -- 64
     {ctype='unsigned char',     label='Master Level'},                          -- 65
-    {ctype='bit[1]',            label='Master Breaker'},                        -- 66
+    {ctype='boolbit',           label='Master Breaker'},                        -- 66
     {ctype='bit[15]',           label='_junk3'},                                -- 66
     {ctype='unsigned int',      label='Current Exemplar Points'},               -- 68
     {ctype='unsigned int',      label='Required Exemplar Points'},              -- 6C
@@ -3382,7 +3421,7 @@ func.incoming[0x0C9][0x01] = L{
     {ctype='unsigned char',     label='Sub Job Level'},                         -- 25
     {ctype='unsigned char',     label='Main Job',           fn=job},            -- 26
     {ctype='unsigned char',     label='Master Level'},                          -- 27
-    {ctype='bit[1]',            label='Master Breaker'},                        -- 28
+    {ctype='boolbit',            label='Master Breaker'},                        -- 28
     {ctype='bit[7]',            label='_junk2'},                                -- 28
     {ctype='data[43]',          label='_unknown5'},                             -- 29   At least the first two bytes and the last twelve bytes are junk, possibly more
 }
@@ -3467,7 +3506,7 @@ fields.incoming[0x0DD] = L{
     {ctype='unsigned char',     label='Sub job',            fn=job},            -- 24
     {ctype='unsigned char',     label='Sub job level'},                         -- 25
     {ctype='unsigned char',     label='Master Level'},                          -- 26
-    {ctype='bit[1]',            label='Master Breaker'},                        -- 27
+    {ctype='boolbit',           label='Master Breaker'},                        -- 27
     {ctype='bit[7]',            label='_junk2'},                                -- 27
     {ctype='char*',             label='Name'},                                  -- 28
 }
@@ -3499,7 +3538,7 @@ fields.incoming[0x0DF] = L{
     {ctype='unsigned char',     label='Sub job',            fn=job},            -- 22
     {ctype='unsigned char',     label='Sub job level'},                         -- 23
     {ctype='unsigned char',     label='Master Level'},                          -- 24
-    {ctype='bit[1]',            label='Master Breaker'},                        -- 25
+    {ctype='boolbit',           label='Master Breaker'},                        -- 25
     {ctype='bit[7]',            label='_junk2'},                                -- 25
 }
 
