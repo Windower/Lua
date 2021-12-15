127ef936b48bc2f07f39a0c4c38add9468f299a8
trevor <trevor.sage@protonmail.com>
2021-12-14 19:12:40 -0500

    Moved id conversion to dialog lib

--- addons/pointwatch/message_ids.lua
+++ addons/pointwatch/message_ids.lua
@@ -189,53 +189,18 @@ local messages = {
 local function update_offset(zone_id)
     -- zone to dat ID mapping for Abyssean zones
     local t = {
-        [15] = 6435, -- 23/80
-        [45] = 6465, -- 23/110
-        [132] = 6552, -- 24/69
-        [215] = 6635, -- 25/24
-        [216] = 6636, -- 25/25
-        [217] = 6637, -- 25/26
-        [218] = 6638, -- 25/27
-        [253] = 6673, -- 25/62
-        [254] = 6674, -- 25/63
+        [15] = true,
+        [45] = true,
+        [132] = true,
+        [215] = true,
+        [216] = true,
+        [217] = true,
+        [218] = true,
+        [253] = true,
+        [254] = true,
     }
 
     if t[zone_id] then
-        -- convert dat ID to dat file path
-        local floor = require('math').floor
-        local byte = require('string').byte
-        local dat_id = t[zone_id]
-        local dat_path = windower.ffxi_path
-        local path
-        local vtable = dat_path .. 'VTABLE.DAT'
-        local ftable = dat_path .. 'FTABLE.DAT'
-        local n = 1
-        local rom = dat_path .. 'ROM/'
-        repeat
-            local f = io.open(vtable, 'rb')
-            f:seek('set', dat_id)
-            if byte(f:read(1)) > 0 then
-                local f = io.open(ftable, 'rb')
-                local offset = 2*dat_id
-                f:seek('set', offset)
-                local dat = f:read(2)
-                f:close()
-
-                local packed_16bit = byte(dat, 2) * 256 + byte(dat, 1)
-                local dir = floor(packed_16bit / 128)
-                local file = packed_16bit - dir * 128
-                
-                path = rom .. tostring(dir) .. '/' .. tostring(file) .. '.DAT'
-            end
-            f:close()
-            n = n + 1
-            local d = tostring(n)
-            rom = dat_path .. 'ROM' .. d .. '/'
-            vtable = rom .. 'VTABLE' .. d .. '.DAT'
-            ftable = rom .. 'FTABLE' .. d .. '.DAT'
-        until path or not windower.dir_exists(rom)
-        if not path then print('Could not convert dat ID', dat_id) return end
-
         -- convert dialog entry to dialog ID
         local dialog = require('dialog')
         local search_phrase = string.char(
@@ -246,12 +211,12 @@ local function update_offset(zone_id)
             229,238,186,160,138,130,160,175,160,211,233,236,246,229,242,
             249,186,160,138,131,255,177,128,135
         )
-        local f = io.open(path, 'rb')
+        local f = dialog.open_dat_by_zone_id(zone_id, 'english')
         local dat = f:read('*a')
         f:close()
         local res = dialog.get_ids_matching_entry(dat, search_phrase)
         if #res ~= 1 then
-            print('In pointwatch/message_ids.lua: matched multiple entries')
+            print('In pointwatch/message_ids.lua: matched multiple or no entries.')
             print('Could not update message ID.')
             return
         end
