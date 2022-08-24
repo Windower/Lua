9c3497ccc2396a25766dc23c2107048124343431
Joe Branger <brangerjoe@gmail.com>
2022-08-24 13:46:08 -0600

    Fix comments, bump version

--- addons/finalAlert/finalAlert.lua
+++ addons/finalAlert/finalAlert.lua
@@ -1,3 +1,4 @@
+--[[
 Copyright © 2022, Godchain
 All rights reserved.
 
@@ -9,7 +10,7 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of <addon name> nor the
+    * Neither the name of finalAlert nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
@@ -23,10 +24,11 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
 
 _addon.name = "finalAlert"
 _addon.author = "Godchain (Asura)"
-_addon.version = "1.3"
+_addon.version = "1.4"
 _addon.commands = {"finalAlert", "fa"}
 
 config = require("config")
