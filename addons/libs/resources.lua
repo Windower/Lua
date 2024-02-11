b9d3dd46b4198b2a711c6e25f6dbb90aa806d90e
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2024-02-11 08:19:50 +0100

    Fixed various issues with resources

--- addons/libs/resources.lua
+++ addons/libs/resources.lua
@@ -1,16 +1,16 @@
 --[[
-    A library to handle ingame resources, as provided by the Radsources XMLs. It will look for the files in Windower/plugins/resources.
+    A library to handle ingame resources, as provided by ResourceExtractor. It will look for the files in Windower/res/.
 ]]
 
 _libs = _libs or {}
 
 require('functions')
 require('tables')
+require('sets')
 require('strings')
+require('bit')
 
 local functions, table, string = _libs.functions, _libs.tables, _libs.strings
-local files = require('files')
-local xml = require('xml')
 
 local fns = {}
 
@@ -70,16 +70,7 @@ end
 resource_mt.__class = 'Resource'
 
 resource_mt.__index = function(t, k)
-    local res = slots[t] and slots[t]:contains(k) and resource_group:endapply(k)
-
-    if not res then
-        res = table[k]
-        if class(res) == 'Resource' then
-            slots[res] = slots[t]
-        end
-    end
-
-    return res
+    return slots[t] and slots[t]:contains(k) and resource_group:endapply(k) or table[k]
 end
 
 resource_mt.__tostring = function(t)
@@ -89,32 +80,43 @@ end
 local resources_path = windower.windower_path .. 'res/'
 
 local flag_cache = {}
-local parse_flags = function(bits, lookup, values)
-    flag_cache[lookup] = flag_cache[lookup] or {}
 
-    if values and not flag_cache[lookup][bits] and lookup[bits] then
-        flag_cache[lookup][bits] = S{lookup[bits]}
-    elseif not flag_cache[lookup][bits] then
-        local res = S{}
-
-        local rem
-        local num = bits
-        local count = 0
-        while num > 0 do
-            num, rem = (num/2):modf()
-            if rem > 0 then
-                res:add(values and lookup[2^count] or count)
-            end
-            count = count + 1
-        end
-
-        flag_cache[lookup][bits] = res
+local check_flags = function(bits, lookup)
+    if lookup.combination == 'replace' and lookup.values[bits] ~= nil then
+        return S{lookup.values[bits]}
     end
 
-    return flag_cache[lookup][bits]
+    local flags = S{}
+
+    for flag, value in pairs(lookup.values) do
+        if bit.band(bits, flag) ~= 0 then
+            flags:add(value)
+        end
+    end
+
+    return flags
 end
 
-local language_strings = S{'english', 'japanese', 'german', 'french'}
+local parse_flags = function(bits)
+    local flags = S{}
+
+    local count = 0
+    repeat
+        local flag = 2 ^ count
+        if bit.band(flag, bits) ~= 0 then
+            flags:add(count)
+        end
+        count = count + 1
+    until flag > bits
+
+    return flags
+end
+
+local get_flags = function(bits, attribute, lookup)
+    flag_cache[attribute] = flag_cache[attribute] or {}
+    flag_cache[attribute][bits] = flag_cache[attribute][bits] or lookup and check_flags(bits, lookup) or parse_flags(bits)
+    return flag_cache[attribute][bits]
+end
 
 -- Add resources from files
 local post_process
@@ -129,33 +131,13 @@ for res_name in res_names:it() do
     end
 end
 
-local lookup = {}
-local flag_keys = S{
-    'flags',
-    'targets',
-}
+local bit_slots = {}
 local fn_cache = {}
 
 post_process = function(t)
     local slot_set = slots[t]
-    for key in slot_set:it() do
-        if lookup[key] then
-            if flag_keys:contains(key) then
-                fn_cache[key] = function(flags)
-                    return parse_flags(flags, lookup[key], true)
-                end
-            else
-                fn_cache[key] = function(flags)
-                    return parse_flags(flags, lookup[key], false)
-                end
-            end
-
-        elseif lookup[key .. 's'] then
-            fn_cache[key] = function(value)
-                return value
-            end
-
-        end
+    for key in slot_set:filter(table.get+{bit_slots}):it() do
+        fn_cache[key] = get_flags-{key, bit_slots[key] ~= true and bit_slots[key] or nil}
     end
 
     for _, entry in pairs(t) do
@@ -171,42 +153,45 @@ post_process = function(t)
     end
 end
 
-lookup = {
-    elements = resources.elements,
-    jobs = resources.jobs,
-    slots = resources.slots,
-    races = resources.races,
-    skills = resources.skills,
+bit_slots = {
+    jobs = true,
+    slots = true,
+    races = true,
     targets = {
-        [0x01] = 'Self',
-        [0x02] = 'Player',
-        [0x04] = 'Party',
-        [0x08] = 'Ally',
-        [0x10] = 'NPC',
-        [0x20] = 'Enemy',
+        combination = 'replace',
+        values = {
+            [0x01] = 'Self',
+            [0x02] = 'Player',
+            [0x04] = 'Party',
+            [0x08] = 'Ally',
+            [0x10] = 'NPC',
+            [0x20] = 'Enemy',
 
-        [0x60] = 'Object',
-        [0x9D] = 'Corpse',
+            [0x60] = 'Object',
+            [0x9D] = 'Corpse',
+        },
     },
     flags = {
-        [0x0001] = 'Flag00',
-        [0x0002] = 'Flag01',
-        [0x0004] = 'Flag02',
-        [0x0008] = 'Flag03',
-        [0x0010] = 'Can Send POL',
-        [0x0020] = 'Inscribable',
-        [0x0040] = 'No Auction',
-        [0x0080] = 'Scroll',
-        [0x0100] = 'Linkshell',
-        [0x0200] = 'Usable',
-        [0x0400] = 'NPC Tradeable',
-        [0x0800] = 'Equippable',
-        [0x1000] = 'No NPC Sale',
-        [0x2000] = 'No Delivery',
-        [0x4000] = 'No PC Trade',
-        [0x8000] = 'Rare',
+        values = {
+            [0x0001] = 'Flag00',
+            [0x0002] = 'Flag01',
+            [0x0004] = 'Flag02',
+            [0x0008] = 'Flag03',
+            [0x0010] = 'Can Send POL',
+            [0x0020] = 'Inscribable',
+            [0x0040] = 'No Auction',
+            [0x0080] = 'Scroll',
+            [0x0100] = 'Linkshell',
+            [0x0200] = 'Usable',
+            [0x0400] = 'NPC Tradeable',
+            [0x0800] = 'Equippable',
+            [0x1000] = 'No NPC Sale',
+            [0x2000] = 'No Delivery',
+            [0x4000] = 'No PC Trade',
+            [0x8000] = 'Rare',
 
-        [0x6040] = 'Exclusive',
+            [0x6040] = 'Exclusive',
+        },
     },
 }
 
