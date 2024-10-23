b6e30704ab6ee6042deabe6bcb54d4894306fe19
Nifim <TesaRollens@gmail.com>
2024-10-23 07:01:40 -0700

    resolve PR feedback for `box_list.lua`   * rename `_byValue` to `_by_value`   * fix type in name `_ordered`   * remove unecessary if conditions.

--- addons/switch_focus/box_list.lua
+++ addons/switch_focus/box_list.lua
@@ -1,50 +1,46 @@
 local box_list = {
-    _orderd = {},
-    _byValue = {},
+    _ordered = {},
+    _by_value = {},
 
     len = function(self)
-        return #self._orderd
+        return #self._ordered
     end,
 
     add = function(self, value)
-        if self._byValue[value] then
+        if self._by_value[value] then
             return
         end
 
-        table.insert(self._orderd, value)
-        table.sort(self._orderd)
+        table.insert(self._ordered, value)
+        table.sort(self._ordered)
 
-        self._byValue = {}
-        for k, v in ipairs(self._orderd) do
-            self._byValue[v] = k
+        self._by_value = {}
+        for k, v in ipairs(self._ordered) do
+            self._by_value[v] = k
         end
     end,
 
     remove = function(self, value)
-        local index = self._byValue[value]
+        local index = self._by_value[value]
         if index == nil then
             return
         end
 
-        table.remove(self._orderd, index)
+        table.remove(self._ordered, index)
 
-        self._byValue = {}
-        for k, v in ipairs(self._orderd) do
-            self._byValue[v] = k
+        self._by_value = {}
+        for k, v in ipairs(self._ordered) do
+            self._by_value[v] = k
         end
     end,
 
     get_index_of = function(self, value)
-        if self._byValue[value] then
-            return self._byValue[value]
-        end
+            return self._by_value[value]
     end
 }
 local meta = {
     __index = function(t,k)
-        if t._orderd[k] then
-            return t._orderd[k]
-        end
+            return t._ordered[k]
     end,
 }
 setmetatable(box_list, meta)
