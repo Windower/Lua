d4f4e149c8b2519ca7dc5abfe7c2fe65b17ec6a7
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2025-01-20 01:23:20 +0100

    Boxdestroyer: Version bump

--- addons/boxdestroyer/boxdestroyer.lua
+++ addons/boxdestroyer/boxdestroyer.lua
@@ -28,7 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 -- addon information
 
 _addon.name = 'boxdestroyer'
-_addon.version = '1.0.6'
+_addon.version = '2.0.0'
 _addon.command = 'boxdestroyer'
 _addon.author = 'Seth VanHeulen (Acacia@Odin)'
 
