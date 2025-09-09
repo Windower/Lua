6aca2ca2c8bd70501ef5c17344e6f7f86c258664
LycorisXI <lycorisxixi@gmail.com>
2025-09-10 00:43:23 +0900

    [DistancePlus] Fixed an error in distance calculation for Flourishes II.

--- addons/DistancePlus/DistancePlus.lua
+++ addons/DistancePlus/DistancePlus.lua
@@ -122,16 +122,17 @@ function displayabilities(distance,master_pet_distance,s,t)
     local list = 'Abilities:\n'
     if abilitylist then 
       for key,ability in pairs(abilitylist) do
-        ability_en = res.job_abilities[ability].name
+        ability_en = res.job_abilities[ability].en
+        ability_name = res.job_abilities[ability].name
         ability_type = res.job_abilities[ability].type
         ability_targets = res.job_abilities[ability].targets
         ability_distance = res.job_abilities[ability].range
-        if distance and ability_en and (ability_type == 'JobAbility' or ability_type == 'PetCommand' or ability_type == 'BloodPactRage' or ability_type == 'BloodPactWard' or ability_type == 'Monster' or ability_type == 'Step') and ability_en ~= "Flourishes II" then 
+        if distance and ability_name and (ability_type == 'JobAbility' or ability_type == 'PetCommand' or ability_type == 'BloodPactRage' or ability_type == 'BloodPactWard' or ability_type == 'Monster' or ability_type == 'Step') and ability_en ~= "Flourishes II" then 
             if ability_targets.Self ~= true then
                 if ability_distance ~= 0 and distance < (t.model_size + ability_distance * range_mult[ability_distance] + s.model_size) and distance ~= 0 then 
-                    list = list..'\\cs(0,255,0)'..ability_en..'\\cs(255,255,255)'..'\n'
+                    list = list..'\\cs(0,255,0)'..ability_name..'\\cs(255,255,255)'..'\n'
                 else
-                    list = list..'\\cs(255,255,255)'..ability_en..'\n'
+                    list = list..'\\cs(255,255,255)'..ability_name..'\n'
                 end
             --[[ too much crap on screen!!! 
             elseif ability_targets.Self == true and (ability_type == 'Monster' or ability_type == 'PetCommand') and master_pet_distance then
