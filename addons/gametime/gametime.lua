e16eece92c4a878b63a63663c265ea5f9d042b8e
gammaleak <benibex@gmx.com>
2025-11-24 11:17:07 -0600

    Make fix of //gt days <arg2> parse even less ugly

--- addons/gametime/gametime.lua
+++ addons/gametime/gametime.lua
@@ -452,7 +452,6 @@ windower.register_event('addon command', function (...)
             log('Showing time display.')
         end
     elseif args[1] == 'days' then
-        local asnum = tonumber(args[2])
         if args[2] == 'alpha' then
             inalpha = tostring(args[3]):zfill(3)
             inalpha = inalpha+0
@@ -460,22 +459,27 @@ windower.register_event('addon command', function (...)
                 gt.gtd:alpha(inalpha)
                 log('Day transparency set to '..inalpha..' ('..math.round(100-(inalpha/2.55),0)..'%).')
             end
-        elseif asnum and asnum > 0 and asnum < 9 then
-            gt.numdays = asnum
-            settings.numdays = asnum
-            day_change(windower.ffxi.get_info().day)
         elseif args[2] == 'x' or args[2] == 'posx' then
             windower.send_command('gt daysx '..args[3])
         elseif args[2] == 'y' or args[2] == 'posy' then
             windower.send_command('gt daysy '..args[3])
+        elseif args[2] == 'show' then
+            gt.gtd:show()
+            log('Showing day display.')
         elseif args[2] == 'hide' then
             gt.gtd:hide()
             log('Day display hidden.')
         elseif args[2] == 'reset' then
             gt.gtd:pos(0,0)
         else
-            gt.gtd:show()
-            log('Showing day display.')
+            local asnum = tonumber(args[2])
+            if not asnum or asnum < 1 or asnum > 8 then
+                log('Invalid command for //gt days')
+            else
+                gt.numdays = asnum
+                settings.numdays = asnum
+                day_change(windower.ffxi.get_info().day)
+            end
         end
     elseif args[1] == 'axis' then
         if args[2] == 'vertical' then
