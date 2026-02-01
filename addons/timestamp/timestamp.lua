8a1ec7892232c36490763a67941893762e36ba05
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2026-02-01 13:24:39 +0100

    Revert "Timestamp: Refactored"
    This reverts commit 151475fc45f0b7670d1e8ae46df67ac10e5dcdc2.

--- addons/timestamp/timestamp.lua
+++ addons/timestamp/timestamp.lua
@@ -1,28 +1,55 @@
+--[[
+timestamp v1.20131102
+
+Copyright © 2013-2014, Giuliano Riccio
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of timestamp nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Giuliano Riccio BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
+
 _addon.name     = 'timestamp'
 _addon.author   = 'Zohno'
-_addon.version  = '2.0.0'
+_addon.version  = '1.20131102'
 _addon.commands = {'timestamp', 'ts'}
 
-require('chat')
+chars = require('chat.chars')
 require('logger')
 require('tables')
 require('sets')
 require('lists')
-require('strings')
-local chars = require('chat.chars')
-local config = require('config')
 
-local tz
-local tz_sep
+config = require('config')
+
 do
     local now  = os.time()
     local h, m = math.modf(os.difftime(now, os.time(os.date('!*t', now))) / 3600)
 
-    tz = ('%+.4d'):format(100 * h + 60 * m)
-    tz_sep = ('%+.2d:%.2d'):format(h, 60 * m)
+    tz = '%+.4d':format(100 * h + 60 * m)
+    tz_sep = '%+.2d:%.2d':format(h, 60 * m)
 end
 
-local constants = {
+constants = {
     ['year']         = '%Y',
     ['y']            = '%Y',
     ['year_short']   = '%y',
@@ -59,19 +86,20 @@ local constants = {
     ['rfc3339']      = '%Y-%m-%dT%H:%M:%S' .. tz_sep,
 }
 
-local newline_pattern = '[\n\007]'
+lead_bytes = S{0x1E, 0x1F, 0xF7, 0xEF, 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x7F}
+newline_pattern = '[' .. string.char(0x07, 0x0A) .. ']'
 
-local defaults = {}
+defaults = {}
 defaults.color  = 201
 defaults.format = '[${time}]'
 
-local settings = config.load(defaults)
+settings = config.load(defaults)
 
-local make_timestamp = function(format)
+function make_timestamp(format)
     return os.date((format:gsub('%${([%l%d_]+)}', constants)))
 end
 
-windower.register_event('incoming text', function(_, modified, mode, newmode, blocked)
+windower.register_event('incoming text', function(original, modified, mode, newmode, blocked)
     if blocked then
         return
     end
@@ -79,19 +107,40 @@ windower.register_event('incoming text', function(_, modified, mode, newmode, bl
     if mode == 151 or mode == 150 then
         newmode = 151
     else
-        local lines = modified:split(newline_pattern, string.encoding.shift_jis, 0, false, false):filter(-'')
+        local lines = L{}
 
-        -- Insert spaces in NPC text
-        if mode == 190 then
-            for i = 2, lines.n do
-                lines[i] = string.char(0x81, 0x40) .. lines[i]
+        -- Split by newline, if applicable
+        if modified:match(newline_pattern) then
+            local split = modified:split(newline_pattern, 0, true, false)
+            lines:append(split[1])
+
+            for i = 2, split.n, 2 do
+                local last = lines:last()[-1]
+                if last and lead_bytes:contains(last:byte()) then
+                    lines[-1] = '%s%s%s':format(lines[-1], split[i], split[i+1])
+                else
+                    lines:append(split[i + 1])
+                end
             end
+
+            if lines:last() == '' then
+                lines:remove(lines.n)
+            end
+
+            -- Insert spaces in NPC text
+            if mode == 190 then
+                for i = 2, lines.n do
+                    lines[i] = string.char(0x81, 0x40) .. lines[i]
+                end
+            end
+        else
+            lines:append(modified)
         end
 
         -- Append the colored timestamp before every line and concatenate them again by a newline
         modified = lines:map(function(str)
             return make_timestamp(settings.format):color(settings.color)..' '..str
-        end):concat('\n')
+        end):concat(string.char(0x0A))
     end
 
     return modified, newmode
@@ -152,17 +201,3 @@ windower.register_event('addon command', function(cmd, ...)
         log(chars.wsquare..' timestamp format <format> -- sets the timestamp format.')
     end
 end)
-
---[[
-Copyright 2013-2026 Giuliano Riccio
-
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-]]
