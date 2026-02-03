66164a035027a12eccdcddfb05107e4b7a506657
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2026-02-03 09:56:08 +0100

    SetTarget: Updated to work with index as well

--- addons/SetTarget/SetTarget.lua
+++ addons/SetTarget/SetTarget.lua
@@ -1,22 +1,25 @@
+_addon.name = 'SetTarget'
+_addon.author = 'Windower'
+_addon.commands = {'settarget', 'st'}
+_addon.version = '2.0.0'
+
 local packets = require('packets')
 
-_addon.name = 'SetTarget'
-_addon.author = 'Arcon'
-_addon.commands = {'settarget', 'st'}
-_addon.version = '1.0.0.0'
-
-windower.register_event('addon command', function(id)
-    id = tonumber(id)
+windower.register_event('addon command', function(str)
+    local id = tonumber(str)
     if id == nil then
         return
     end
 
-    local target = windower.ffxi.get_mob_by_id(id)
-    if not target then
+    local target = windower.ffxi.get_mob_by_id(id) or windower.ffxi.get_mob_by_index(id)
+    if not target or target.distance > 2500 then
         return
     end
 
     local player = windower.ffxi.get_player()
+    if not player then
+        return
+    end
 
     packets.inject(packets.new('incoming', 0x058, {
         ['Player'] = player.id,
@@ -24,3 +27,17 @@ windower.register_event('addon command', function(id)
         ['Player Index'] = player.index,
     }))
 end)
+
+--[[
+Copyright 2015-2026 Windower
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+]]
