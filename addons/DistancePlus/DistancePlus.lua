b8981e7276f851a2e20b6f02b0a438f5c6000d38
Voli and Az <81529384+voliathon@users.noreply.github.com>
2026-02-17 01:03:53 -0500

    Add files via upload
    Updated the copyright to 2026

--- addons/DistancePlus/DistancePlus.lua
+++ addons/DistancePlus/DistancePlus.lua
@@ -1,5 +1,6 @@
 --[[
 Copyright © 2017, Sammeh of Quetzalcoatl
+Copyright © 2026, Voliathon of Bahamut
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -17,7 +18,7 @@ modification, are permitted provided that the following conditions are met:
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Sammeh BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL Sammeh OR CONTRIBUTORS BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -27,8 +28,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
 _addon.name = 'DistancePlus'
-_addon.author = 'Sammeh'
-_addon.version = '2.0.2'
+_addon.author = 'Sammeh, Voliathon'
+_addon.version = '2.0.1'
 _addon.command = 'dp'
 
 require('tables')
