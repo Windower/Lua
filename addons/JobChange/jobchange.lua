a28bff1d6547a71592480037616e5a4eccd42fba
GoblinGuide <12886590+GoblinGuide@users.noreply.github.com>
2026-04-16 11:57:39 -0400

    remove space from "Addon Name" field

--- addons/JobChange/jobchange.lua
+++ addons/JobChange/jobchange.lua
@@ -26,7 +26,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
 
-_addon.name = 'Job Change'
+_addon.name = 'JobChange'
 _addon.author = 'Sammeh; Akaden'
 _addon.version = '1.0.4'
 _addon.command = 'jc'
